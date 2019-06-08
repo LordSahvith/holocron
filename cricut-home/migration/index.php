778e2a9815e456e23a9fcd6e2353ab71aa4b0a04
Caleb Anderon <[bulletproofchild@gmail.com](mailto:bulletproofchild@gmail.com)>
2019-06-08 10:48:48 -0600

    delete unnecessary folders

--- cricut-home/migration/index.php
+++
@@ -1,5 +0,0 @@
-<?php include 'templates/header.php'; ?>
-
-<?php include 'templates/nav.php'; ?>
-
-<?php include 'templates/footer.php'; ?>
--- cricut-home/migration/templates/nav.php
+++
@@ -1,5 +0,0 @@
-<nav style="height: 70px; width: 100%;">
-  <a href="../experience.php">Experience</a>
-  <a href="../maker.php">Maker</a>
-  <a href="../explore-air.php">Explore</a>
-</nav>
