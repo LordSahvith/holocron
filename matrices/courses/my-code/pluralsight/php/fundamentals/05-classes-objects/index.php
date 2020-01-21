87a69d2e31eeea1c620861c9acfbc2229271b188
Caleb Anderson <canderson@cricut.com>
2020-01-21 10:07:37 -0700

    create new class to keep all examples from throwing errors

--- matrices/courses/my-code/pluralsight/php/fundamentals/05-classes-objects/index.php
+++ matrices/courses/my-code/pluralsight/php/fundamentals/05-classes-objects/index.php
@@ -4,6 +4,39 @@ namespace index;
 
 echo "<h1>PHP Classes & Objects</h1>";
 
+class Person1 
+{
+    const AVG_LIFE_SPAN = 79;
+
+    public $firstName;
+    public $lastName;
+    public $yearBorn;
+
+    function __construct($firstName = "", $lastName = "", $yearBorn = "")
+    {
+        echo "Person Constructor.".PHP_EOL;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->yearBorn = $yearBorn;
+    }
+
+    public function getFirstName() 
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName($tempName)
+    {
+        $this->firstName = $tempName;
+    }
+    
+    public function getFullName()
+    {
+        echo "Person-getFullName()<br />";
+        return $this->firstName . " " . $this->lastName;
+    }
+}
+
 class Person 
 {
     const AVG_LIFE_SPAN = 79;
@@ -75,35 +108,33 @@ class Author extends Person
     }
 }
 
-$myObject = new Person("Lord", "Sahvith", "3350bby");
+$myObject = new Person1("Lord", "Sahvith", "3350bby");
 
 echo $myObject::AVG_LIFE_SPAN;
 echo "<br />";
-// echo $myObject->firstName;
+echo $myObject->firstName;
 echo "<br />";
 
-// $myObject->firstName = "Darth";
-// echo $myObject->firstName . " ";
-// echo $myObject->lastName . " ";
-// echo $myObject->yearBorn;
+$myObject->firstName = "Darth";
+echo $myObject->firstName . " ";
+echo $myObject->lastName . " ";
+echo $myObject->yearBorn;
 echo "<br />";
 
 $myObject->setFirstName("Your Holiness");
-// echo $myObject->firstName;
+echo $myObject->firstName;
 echo "<br />";
 
-$secObject = new Person("Billy", "Bob", 1945);
+$secObject = new Person1("Billy", "Bob", 1945);
 $secObject->setFirstName("billy");
-// echo $secObject->firstName;
-// echo $secObject->yearBorn;
+echo $secObject->firstName;
+echo $secObject->yearBorn;
 echo "<br />";
 
 $thirdObject = new Person();
 $thirdObject->setFirstName("Another one");
 echo $thirdObject->getFirstName();
 echo "<br />";
-
-
 
 echo "<h2>Static/Public/Protected/Private</h2>";
 
@@ -115,5 +146,13 @@ echo "<br />";
 echo $newAuthor->getSelfStaticProperty();
 echo "<br />";
 echo $newAuthor->getParentStaticProperty();
+echo "<br />";
+
+echo "<h2>Scope Resolution Operator</h2>";
+echo Person::AVG_LIFE_SPAN;
+echo "<br />";
+echo Person::$test;
+echo "<br />";
+echo Author::getSelfStaticProperty();
 
 ?>
