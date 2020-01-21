b6e0ec1caa26f459c4afbea50935523ed86f814b
Caleb Anderson <canderson@cricut.com>
2020-01-21 16:11:39 -0700

    use parent::__construct()

--- matrices/courses/my-code/pluralsight/php/fundamentals/05-classes-objects/main.php
+++ matrices/courses/my-code/pluralsight/php/fundamentals/05-classes-objects/main.php
@@ -4,7 +4,7 @@ include_once "Author.php";
 
 echo "<h1>PHP Include Files</h1>";
 
-$newAuthor = new Author("Lord", "Sahvith", "3350bby");
+$newAuthor = new Author("Your Holiness", "Lord", "Sahvith", "3350bby");
 echo $newAuthor->getFullName();
 $newAuthor->setFirstName("Darth");
 echo $newAuthor->getFullName();
