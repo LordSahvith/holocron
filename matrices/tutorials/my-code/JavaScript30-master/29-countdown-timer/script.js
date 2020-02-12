bd34b7ae6a5bd73525d1bcd267e0bc1911b46cf6
Caleb Anderson <canderson@cricut.com>
2020-02-12 08:25:41 -0700

    good stopping point

---
+++ matrices/tutorials/my-code/JavaScript30-master/29-countdown-timer/script.js
@@ -0,0 +1,36 @@
+let countdown;
+const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
+
+function timer(seconds) {
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    displayEndTime(then);
+    
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        // check if we should stop
+        if (secondsLeft < 0) {
+            clearInterval(countdown);
+            return;
+        }
+        // display it
+        displayTimeLeft(secondsLeft);
+    }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+    timerDisplay.textContent = display;
+    console.log({minutes, remainderSeconds});
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hours = end.getHours();
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be back at ${hours > 12 ? hours - 12 : hours}:${minutes}`
+}
