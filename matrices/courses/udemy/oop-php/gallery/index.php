6cd1b3d6705a3978a9a6eeb2948dc9642effe01b
Caleb Anderson <canderson@cricut.com>
2020-02-21 09:34:48 -0700

    separate out sections

--- matrices/courses/udemy/oop-php/gallery/index.php
+++ matrices/courses/udemy/oop-php/gallery/index.php
@@ -1,30 +1,19 @@
 <?php include("includes/header.php"); ?>
 
 
-        <div class="row">
+<div class="row">
 
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
+    <!-- Blog Entries Column -->
+    <div class="col-md-8">
 
-    
-            
-          
-         
+    </div>
 
-            </div>
+    <!-- Blog Sidebar Widgets Column -->
+    <div class="col-md-4">
 
+        <?php include("includes/sidebar.php"); ?>
 
+    </div>
+    <!-- /.row -->
 
-
-            <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
-
-            
-                 <?php include("includes/sidebar.php"); ?>
-
-
-
-        </div>
-        <!-- /.row -->
-
-        <?php include("includes/footer.php"); ?>
+<?php include("includes/footer.php"); ?>
