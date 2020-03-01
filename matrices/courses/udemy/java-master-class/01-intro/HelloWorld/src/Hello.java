198933f121fc91cee382a7d78de8db8067ce0e9b
Caleb <bulletproofchild@gmail.com>
2020-03-01 13:43:31 -0700

    declaring & assigning variables

--- matrices/courses/udemy/java-master-class/01-intro/HelloWorld/src/Hello.java
+++ matrices/courses/udemy/java-master-class/01-intro/HelloWorld/src/Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Lord Sahvith!");
+
+        // declaration statement which initializes variable
+        int myFirstNumber = 4; // int is whole number (i.e. no decimals)
+        System.out.println(myFirstNumber); // creates new line after printing
+        System.out.print(myFirstNumber); // doesn't create new line after printing
+        System.out.print(myFirstNumber); // this should print right after above
     }
 }
