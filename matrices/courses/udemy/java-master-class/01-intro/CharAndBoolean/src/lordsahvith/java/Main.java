99131eff27f0e8885435193d18837a1f6a87ee5a
Caleb <bulletproofchild@gmail.com>
2020-03-01 19:59:11 -0700

    Primitive Data Types: Challenge - Float and Double

---
+++ matrices/courses/udemy/java-master-class/01-intro/CharAndBoolean/src/lordsahvith/java/Main.java
@@ -0,0 +1,9 @@
+package lordsahvith.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // char = 16 bits of memory (single character)
+        char myChar = 'D';
+    }
+}
