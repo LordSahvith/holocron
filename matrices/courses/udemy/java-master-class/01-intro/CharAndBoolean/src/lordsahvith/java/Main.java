95053b0deb03094d59195296ae3f9fe4b443f46e
Caleb <bulletproofchild@gmail.com>
2020-03-02 20:17:13 -0700

    Primitive Data Types: Char and Boolean

--- matrices/courses/udemy/java-master-class/01-intro/CharAndBoolean/src/lordsahvith/java/Main.java
+++ matrices/courses/udemy/java-master-class/01-intro/CharAndBoolean/src/lordsahvith/java/Main.java
@@ -5,5 +5,13 @@ public class Main {
     public static void main(String[] args) {
         // char = 16 bits of memory (single character)
         char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        System.out.println(myCopyrightChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
     }
 }
