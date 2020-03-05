f953f69929710377bd0bdf1337ee09d4ae0d41a8
Caleb Anderson <canderson@cricut.com>
2020-03-05 16:20:07 -0700

    start and finish section 6

---
+++ matrices/courses/pluralsight/php/fundamentals/06-operators-control-structures/01-arithmetic.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "addition: 8 + 3 = ";
+echo 8 + 3 . PHP_EOL;
+echo "subtraction: 8 - 3 = ";
+echo 8 - 3 . PHP_EOL;
+echo "mupltiplication: 8 * 3 = ";
+echo 8 * 3 . PHP_EOL;
+echo "division: 8 / 3 = ";
+echo 8 / 3 . PHP_EOL;
+
+echo "modulus: 8 % 3 = ";
+echo 8 % 3 . PHP_EOL;
+
+echo "exponent: 4 ^ 8 = ";
+echo 4 ** 8 . PHP_EOL;
