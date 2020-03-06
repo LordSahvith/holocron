46990ad8a541fc94e05c56b57cbd20ecb27d368c
Caleb Anderson <canderson@cricut.com>
2020-03-05 18:15:25 -0700

    finish spaceship operator video

---
+++ matrices/courses/pluralsight/php/fundamentals/06-operators-control-structures/06-spaceship-operator.php
@@ -0,0 +1,10 @@
+<?php
+
+// determines whether the check is less than, equal, or greater
+// by printing out -1 for less, 0 for equal or 1 for greater
+echo 1 <=> 4;
+echo PHP_EOL;
+echo 4 <=> 1;
+echo PHP_EOL;
+echo 4 <=> 4;
+echo PHP_EOL;
