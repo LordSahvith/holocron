13d1e12387eb7d1c3fb34f8f1c50a454c0876569
Caleb Anderson <canderson@cricut.com>
2020-10-07 18:38:40 -0600

    merge this into dev/master, adds index.php files to help navigate

---
+++ matrices/courses/pluralsight/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Holocron Courses</title>
+</head>
+<body>
+
+    <ul>
+        <li><a href="css/index.php">CSS</a></li>
+        <li><a href="js/index.php">JS</a></li>
+        <li><a href="php/index.php">PHP</a></li>
+    </ul>
+    
+</body>
+</html>
