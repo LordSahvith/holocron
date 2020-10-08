b8a5021626e28e0179098615d0fde0943e6ba886
Caleb Anderson <canderson@cricut.com>
2020-10-07 19:00:15 -0600

    start the PHP: getting started course

---
+++ matrices/courses/pluralsight/php/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Holocron Courses</title>
+</head>
+<body>
+
+    <ul>
+        <li><a href="fundamentals/index.php">Fundamentals</a></li>
+        <li><a href="getting-started/index.php">Getting Started</a></li>
+    </ul>
+    
+</body>
+</html>
