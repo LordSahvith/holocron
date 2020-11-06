45b7645a2132cdf5b7fe0b331af762d9b0f671b7
Caleb Anderson <canderson@cricut.com>
2020-11-05 20:21:25 -0700

    delete user

--- matrices/courses/pluralsight/php/getting-started/06-db-management/select.php
+++ matrices/courses/pluralsight/php/getting-started/06-db-management/select.php
@@ -12,10 +12,13 @@ $result = $db->query($sql);
 
 foreach ($result as $row) {
     printf(
-        '<li><span style="color: %s">%s (%s)</span>',
+        '<li><span style="color: %s">%s (%s)</span> 
+        <a href="delete.php?id=%s">Delete</a>
+        </li>',
         htmlspecialchars($row['color'], ENT_QUOTES),
         htmlspecialchars($row['name'], ENT_QUOTES),
-        htmlspecialchars($row['gender'], ENT_QUOTES)
+        htmlspecialchars($row['gender'], ENT_QUOTES),
+        htmlspecialchars($row['id'], ENT_QUOTES)
     );
 }
 
