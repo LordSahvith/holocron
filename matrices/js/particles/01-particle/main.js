32205335550b1c784f2be6188ab7cb47fd97b164
Caleb Anderson <canderson@cricut.com>
2021-01-19 16:38:29 -0700

    adding velocity and gravity

--- matrices/js/particles/01-particle/main.js
+++ matrices/js/particles/01-particle/main.js
@@ -4,23 +4,19 @@ const context = canvas.getContext('2d');
 // black background
 context.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
 
-// square particle
-context.fillStyle = 'white';
-context.fillRect(300, 200, 10, 10);
-
-// round particle
-context.beginPath();
-context.fillStyle = 'white';
-context.arc(500, 200, 10, 0, Math.PI*2, true);
-context.closePath();
-context.fill();
-
 let posX = 20;
 let posY = 100;
+let velX = 10;
+let velY = -10;
+let gravity = 1;
 
 setInterval(function() {
-    posX += 1;
-    posY += 0.25;
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+    posX += velX;
+    posY += velY;
+    velY += gravity;
 
     context.beginPath();
     context.fillStyle = 'white';
