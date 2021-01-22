444567dc934377a33a371983212007069c2968c1
Caleb Anderson <canderson@cricut.com>
2021-01-22 13:06:02 -0700

    particle that creates water looking effect with svgs

---
+++ matrices/js/particles/03-particle/03-particle/main.js
@@ -0,0 +1,81 @@
+const canvas = document.querySelector('#myCanvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let particleArray = [];
+let numberOfParticles = 300;
+
+
+// get mouse position
+const mouse = {
+    x: null,
+    y: null
+};
+
+window.addEventListener('mousemove', function(event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+});
+
+window.addEventListener('mouseout', function() {
+    mouse.x = undefined;
+    mouse.y = undefined;
+});
+
+// create prarticles
+class Particle {
+    constructor(x, y, size, color, weight) {
+        this.x = x;
+        this.y = y; 
+        this.size = size;
+        this.color = color; 
+        this.weight = weight;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+
+    update() {
+        this.size -= 0.1;
+        if (this.size < 0) {
+            this.x = (mouse.x + ((Math.random() * 20) - 10));
+            this.y = (mouse.y + ((Math.random() * 20) - 10));
+            this.size = (Math.random() * 12) + 10;
+            this.weight = (Math.random() * 2) - 0.5;
+        }
+        this.y += this.weight;
+        this.weight += 0.2;
+
+        if ((this.y > canvas.height - this.size) && (this.weight > 0.2)) {
+            this.weight *= -0.2;
+        }
+    }
+}
+
+function init() {
+    particleArray = [];
+    for (let i = 0; i < numberOfParticles; i++) {
+        let x = Math.random() * canvas.width;
+        let y = Math.random() * canvas.height;
+        let size = (Math.random() * 12) + 10;
+        let color = 'white';
+        let weight = 1;
+        particleArray.push(new Particle(x, y, size, color, weight));
+    }
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < particleArray.length; i++) {
+        particleArray[i].update();
+        particleArray[i].draw();
+    }
+    requestAnimationFrame(animate);
+}
+
+init();
+animate();
