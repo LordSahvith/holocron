c06cedf8326c2bab750b68bbf78552342bc01ac3
Caleb Anderson <canderson@cricut.com>
2021-01-26 15:37:31 -0700

    adding particles randomly; could use this for stars

---
+++ matrices/js/particles/05-particle/main.js
@@ -0,0 +1,65 @@
+const canvas = document.querySelector('#myCanvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particleArray = [];
+
+// handle mouse
+const mouse = {
+    x: null,
+    y: null,
+    radius: 150
+};
+
+window.addEventListener('mousemove', function(event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+});
+
+ctx.fillStyle = 'white';
+ctx.font = '30px Verdana';
+ctx.fillText('A', 0, 40);
+const data = ctx.getImageData(0, 0, 100, 100);
+
+class Particle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.size = 3;
+        this.baseX = this.x;
+        this.baseY = this.y;
+        this.density = (Math.random() * 30) + 1;
+    }
+
+    draw() {
+        ctx.fillStyle = 'white';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+        ctx.closePath();
+        ctx.fill();
+    }
+}
+
+function init() {
+    particleArray = [];
+    for (let i = 0; i < 10; i++) {
+        let x = Math.random() * 500;
+        let y = Math.random() * 500;
+        particleArray.push(new Particle(x, y));
+    }
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < particleArray.length; i++) {
+        particleArray[i].draw();
+    }
+
+    requestAnimationFrame(animate);
+}
+
+init();
+animate();
