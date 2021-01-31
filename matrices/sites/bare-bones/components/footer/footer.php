55f9b3d26c632610aa25656a502c28cd99ec5c63
Caleb <bulletproofchild@gmail.com>
2021-01-31 11:55:09 -0700

    started footer; then needed columns

---
+++ matrices/sites/bare-bones/components/footer/footer.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Component - Navbar</title>
+
+
+    <link rel="stylesheet" href="../../css/bare-bones.css">
+    <link rel="stylesheet" href="../../css/components/footer.css">
+</head>
+
+<body>
+
+    <?php include 'footer.html'; ?>
+
+    <script src="../../js/utils.js"></script>
+
+</body>
+
+</html>
