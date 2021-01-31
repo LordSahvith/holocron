2e1f10d41b578b0f610bb2ae433e6b11e804ba78
Caleb <bulletproofchild@gmail.com>
2021-01-31 13:35:04 -0700

    start my base file and add to columns

--- matrices/sites/bare-bones/components/footer/footer.php
+++ matrices/sites/bare-bones/components/footer/footer.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Component - Navbar</title>
+    <title>Component - Footer</title>
 
 
     <link rel="stylesheet" href="../../css/bare-bones.css">
