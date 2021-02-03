deb22f84a124027085fcb3fac0166e8f50b198aa
Caleb <bulletproofchild@gmail.com>
2021-02-03 15:42:26 -0700

    get menu lists to open/close on hover/leave

--- matrices/sites/bare-bones/js/components/menu-list.js
+++ matrices/sites/bare-bones/js/components/menu-list.js
@@ -1,18 +1,18 @@
 const menuListIdentifier = '.bb-menuList';
 const menuListButton = '.bb-menuList-button';
 const menuListContent = '.bb-menuList-contents';
-const arrowButton = '.arrow.down';
 const menuLists = document.querySelectorAll(menuListIdentifier);
 let menuListsArray = [];
 
 class MenuList {
     constructor(menuList) {
-        this.button = menuList.querySelector(menuListButton);
-        this.arrow = menuList.querySelector(arrowButton);
-        this.content = menuList.querySelector(menuListContent);
+        this.menu = menuList;
+        this.button = this.menu.querySelector(menuListButton);
+        this.content = this.menu.querySelector(menuListContent);
     }
 
     toggle() {
+        toggle(this.menu);
         toggle(this.button);
         toggle(this.content);
     }
@@ -22,7 +22,7 @@ class MenuList {
     }
 
     log() {
-        console.log(this.button, this.arrow, this.content);
+        console.log(this.button, this.content);
     }
 }
 
@@ -33,16 +33,34 @@ function createMenuLists(menuList) {
 function setEventListeners() {
     for (let i = 0; i < menuListsArray.length; i++) {
         menuListsArray[i].button.addEventListener('click', () => menuListsArray[i].toggle());
+        menuListsArray[i].menu.addEventListener('mouseover', (e) => toggleOpen(e));
+        menuListsArray[i].menu.addEventListener('mouseleave', (e) => toggleClosed(e));
     }
 }
 
 menuLists.forEach(menuList => createMenuLists(menuList));
 setEventListeners();
 
-window.addEventListener('click', function(e) {
+window.addEventListener('click', function (e) {
     for (let i = 0; i < menuListsArray.length; i++) {
         if (e.target !== menuListsArray[i].button && menuListsArray[i].isOpen()) {
             menuListsArray[i].toggle();
         }
     }
 });
+
+function toggleOpen(e) {
+    for (let i = 0; i < menuListsArray.length; i++) {
+        if (e.target === menuListsArray[i].menu) {
+            menuListsArray[i].toggle();
+        }
+    }
+}
+
+function toggleClosed(e) {
+    for (let i = 0; i < menuListsArray.length; i++) {
+        if ((e.target !== menuListsArray[i].menu || e.target !== menuListsArray[i].content) && menuListsArray[i].isOpen()) {
+            menuListsArray[i].toggle();
+        }
+    }
+}
