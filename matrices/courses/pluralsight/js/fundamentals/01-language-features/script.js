4def375ea1f238b9382645f5c8ab759bfe936336
Caleb Anderson <canderson@cricut.com>
2021-06-10 16:49:37 -0600

    destructuring arrays

--- matrices/courses/pluralsight/js/fundamentals/01-language-features/script.js
+++ matrices/courses/pluralsight/js/fundamentals/01-language-features/script.js
@@ -32,6 +32,7 @@ if (true) {
 }
 console.log("var carId6 after block:", carId6); // 17 - var has no block scope
 
+
 /* * * * * * * *  *
 * REST PARAMETERS *
 * * * * * * * * * */
@@ -49,3 +50,36 @@ function sendCarsOnDay(day, ...allCarIds) {
 
 // sendCars(carId3, carId4, carId5); // error - carId5 is not defined
 sendCars("Monday", 1, 2, 3); // Monday  1  2  3
+
+
+/* * * * * * * * * * * *
+* DESTRUCTURING ARRAYS *
+* * * * * * * * * * *  */
+let carIds = [1, 2, 4];
+let [car1, car2, car3] = carIds;
+console.log(car1, car2, car3); // 1  2  4
+
+let carIds2 = [2, 3, 5];
+// let car1, remainingCars; // error - Identifier 'car1' has already been declared
+let car4, remainingCars;
+[car4, ...remainingCars] = carIds2;
+console.log(car4, remainingCars); // 2  [3, 5]
+
+let carIds3 = [3, 4, 6, 8];
+let remainingCars2;
+[, ...remainingCars2] = carIds3;
+console.log(remainingCars2); // [4, 6, 8]
+
+let carIds4 = [4, 5, 7, 9];
+let remainingCars3;
+[,, ...remainingCars3] = carIds4; // each , skips the index
+console.log(remainingCars3); // [7, 9]
+
+let carIds5 = [5, 6, 8, 10];
+let car5, car6, theRest;
+// [, car2, ...theRest] = car5; // error - undefined is not iterable (cannot read property Symbol(Symbol.iterator)) -- car5 is undefined
+[, car2, ...theRest] = carIds5; 
+console.log(car1, car2, theRest); // 1  6  [8, 10]
+let carIds6 = [6, 7, 9, 11];
+[car5, car6, ...theRest] = carIds6; 
+console.log(car5, car6, theRest); // 6  7  [9, 11]
