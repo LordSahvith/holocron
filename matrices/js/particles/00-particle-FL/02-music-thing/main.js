4845d198f877d6b698ec048a0be8b8ec3f4c2bcf
Caleb Anderson <canderson@cricut.com>
2021-09-17 17:41:46 -0600

    start music thing

---
+++ matrices/js/particles/00-particle-FL/02-music-thing/main.js
@@ -0,0 +1,8 @@
+const button1 = document.querySelector('#button1'); 
+
+let audio1 = new Audio();
+audio1.src = '...';
+
+button1.addEventListener('click', function() {
+    console.log("here");
+});
