e64a665769bcccc00c387bb2d329ab998c445660
Caleb <bulletproofchild@gmail.com>
2021-09-28 19:45:17 -0600

    add class to intro for game time; create be right back

--- matrices/stream-videos/be-right-back/main.js
+++ matrices/stream-videos/be-right-back/main.js
@@ -1,26 +1,3 @@
-function startTimer() {
-    let countDownDuration = 666;
-    let timer = document.querySelector('#countDownTimer');
-    timer.innerHTML = countDownDuration;
-
-    // UPDATE COUNTDOWN EVERY 1 SECOND
-    let countDown = setInterval(function () {
-        timer.innerHTML = --countDownDuration;
-
-        if (countDownDuration <= 0) {
-            clearInterval(countDown);
-            timer.innerHTML = "Game Time!";
-            timer.classList.add('game-time');
-        }
-    }, 1000);
-}
-
-window.onload = function() {
-    startTimer();
-};
-
-// PARTICLE \\
-
 const canvas = document.querySelector('#canvas1');
 const ctx1 = canvas.getContext('2d');
 canvas.width = window.innerWidth;
