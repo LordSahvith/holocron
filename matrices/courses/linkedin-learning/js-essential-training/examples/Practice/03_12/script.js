b50a93e0257992294c0dbbc83d62a7df4cfb1d4f
Caleb Anderson <canderson@cricut.com>
2021-11-18 11:48:13 -0700

    finish 03_12 practice - create a class

--- matrices/courses/linkedin-learning/js-essential-training/examples/Practice/03_12/script.js
+++ matrices/courses/linkedin-learning/js-essential-training/examples/Practice/03_12/script.js
@@ -6,3 +6,36 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+
+import GameConsole from "./GameConsole.js";
+
+const xbox = new GameConsole(
+    "Xbox One X",
+    "black",
+    "AC",
+    "3",
+    {
+        name: "Red Dead Redemption 2",
+        isDisc: false,
+        size: "95gb"
+    },
+    false
+);
+
+const ps4 = new GameConsole(
+    "PS4",
+    "Red",
+    "AC",
+    "3",
+    {
+        name: "Red Dead Redemption 2",
+        isDisc: false,
+        size: "95gb"
+    },
+    false
+);
+
+console.log(xbox);
+console.log(ps4);
+console.log(xbox.startGame());
+console.log(ps4.startGame());
