3f2ac777b68e133eb9da76266fec391589fd6260
Caleb Anderson <canderson@cricut.com>
2021-11-18 10:02:23 -0700

    finish 03_07 practice

--- matrices/courses/linkedin-learning/js-essential-training/examples/Practice/03_07/script.js
+++ matrices/courses/linkedin-learning/js-essential-training/examples/Practice/03_07/script.js
@@ -7,3 +7,23 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const gameConsole = {
+    name: "Xbox One X",
+    color: "black",
+    power: "AC",
+    usbPorts: "3",
+    game: {
+        name: "Red Dead Redemption 2",
+        isDisc: false,
+        size: "95gb"
+    },
+    isRunning: false,
+    startGame: function () {
+        this.isRunning = true;
+        this.startUpMessage(this.game);
+    },
+    startUpMessage: function (game) {
+        console.log(game.name, "has started.");
+    }
+};
