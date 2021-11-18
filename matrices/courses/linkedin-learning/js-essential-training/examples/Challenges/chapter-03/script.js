7e90ce6b0e8b050f9af054e8bf3d1e9c0d9e50e9
Caleb Anderson <canderson@cricut.com>
2021-11-18 12:29:35 -0700

    add another book

--- matrices/courses/linkedin-learning/js-essential-training/examples/Challenges/chapter-03/script.js
+++ matrices/courses/linkedin-learning/js-essential-training/examples/Challenges/chapter-03/script.js
@@ -7,6 +7,17 @@ const lotr = new Book(
     "fantasy"
 );
 
+const nightAngel = new Book(
+    "The Night Angel Trilogy",
+    "paper bound",
+    383,
+    "fantasy"
+);
+
 console.log(lotr);
 console.log(lotr.currentPage = 382);
 console.log(lotr.turnPage());
+
+console.log(nightAngel);
+console.log(nightAngel.currentPage = 382);
+console.log(nightAngel.turnPage());
