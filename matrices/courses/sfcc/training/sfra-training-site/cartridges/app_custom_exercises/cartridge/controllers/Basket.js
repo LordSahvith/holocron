5b886d9ab834c1b02ef9c06fc9e2ae88ecb378d7
Caleb Anderson <canderson@cricut.com>
2021-11-30 16:35:21 -0700

    6-4 using isml loops

--- matrices/courses/sfcc/training/sfra-training-site/cartridges/app_custom_exercises/cartridge/controllers/Basket.js
+++ matrices/courses/sfcc/training/sfra-training-site/cartridges/app_custom_exercises/cartridge/controllers/Basket.js
@@ -2,16 +2,14 @@
 
 var server = require('server');
 //in the code below  require BasketMgr
-var  BasketMgr = 
+var BasketMgr = require('dw/order/BasketMgr');
+
 server.get('Start', function (req, res, next) {
-	
- //in the code below use BasketMgr to get a basket 
-  
-	var basket =
-   
+	//in the code below use BasketMgr to get a basket
+	var basket = BasketMgr.getCurrentBasket();
+
     res.render('showBasket', {Basket:basket});
     next();
-	
 });
 
 module.exports = server.exports();
