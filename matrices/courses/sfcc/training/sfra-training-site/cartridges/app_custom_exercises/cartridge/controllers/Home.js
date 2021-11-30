53a4b38df4a8b5d053e413dc9873572c23be5670
Caleb Anderson <canderson@cricut.com>
2021-11-30 10:29:28 -0700

    use middleware

--- matrices/courses/sfcc/training/sfra-training-site/cartridges/app_custom_exercises/cartridge/controllers/Home.js
+++ matrices/courses/sfcc/training/sfra-training-site/cartridges/app_custom_exercises/cartridge/controllers/Home.js
@@ -3,16 +3,19 @@
 
 var server = require('server');
 //TODO inherit functionality with module.superModule by using the extend method
-
+server.extend(module.superModule);
 
 
 //TODO insert functionality before the Show Route using server.prepend
 server.prepend('Show', function (req, res, next) {
 	//TODO in the code below, get ViewData from res
-	
+    var viewData = res.viewData;
+
     //TODO set a viewData variable 'detailText' with value 'General Company Details, no promo at this time' in the code below 
-	
-	
+	viewData.detailText = "General Company Details, no promo at this time";
+
+    res.setViewData(viewData);
+
 	next();
 });
 
@@ -22,14 +25,20 @@ server.prepend('Show', function (req, res, next) {
  * If the query string has ?promo=1, then it appends the route with different data. 
  */
 //TODO modify the Show Route by using server.append
-
+server.append("Show", function (req, res, next) {
 
 	if (req.querystring.promo == 1) {
 		// This example shows an alternative to getting the view data like you did in prepend
 		res.setViewData(
 			{ detailText: 'Home with Promo (All Bags for Sale this week).' }
 		);
-	
+	}
+
+	if (req.querystring.promo == 2) {
+		// This example shows an alternative to getting the view data like you did in prepend
+		res.setViewData(
+			{ detailText: 'Everything free with the signing of your soul.' }
+		);
 	}
 
 	next();
--- matrices/courses/sfcc/training/sfra-training-site/cartridges/app_custom_exercises/cartridge/controllers/Middleware.js
+++ matrices/courses/sfcc/training/sfra-training-site/cartridges/app_custom_exercises/cartridge/controllers/Middleware.js
@@ -2,13 +2,11 @@
 
 var server = require('server');
 //Define a variable named userLoggedIn that requires the userLoggedIn script
+var userLoggedIn = require('*/cartridge/scripts/middleware/userLoggedIn');
 
-server.get('Start', //Invoke the middleware step that validates login 
-	function (req, res, next) {
-
+server.get('Start', userLoggedIn.validateLoggedIn, function (req, res, next) {
 	res.print('<html><body><h1>You are now a logged in user!</h1></body></html>');
     next();
-	 
 });
 
 module.exports = server.exports();
