d943229cd4c77f80d9500b11ec3ab1ad8f5fdcde
Caleb Anderson <canderson@cricut.com>
2021-12-01 12:53:38 -0700

    6-10:6-11:6-12 rendering a page designer page

--- matrices/courses/sfcc/training/sfra-training-site/cartridges/app_custom_exercises/cartridge/controllers/Sales.js
+++ matrices/courses/sfcc/training/sfra-training-site/cartridges/app_custom_exercises/cartridge/controllers/Sales.js
@@ -2,19 +2,26 @@
 
 var server = require('server');
 
-
 server.get('Show', function (req, res, next) {
 
     //define a variable named PageMgr that requires the dw.experience.PageMgr API
-    
+    var PageMgr = require('dw/experience/PageMgr');
+
     //get the page with the specific, hard-coded id
+    var page = PageMgr.getPage('salepage');
 
     //Hint: Use hasVisibilityRules() and isVisible() checks
-    //Hint: use PageMgr.renderPage method and res object to render the page
-    //Hint: don't forget to cover the else condition, because in our exercise hasVisibilityRules() is false
+    if (page != null && page.hasVisibilityRules()) {
+        if (page.isVisible()) {
+            //Hint: use PageMgr.renderPage method and res object to render the page
+            res.print(PageMgr.renderPage(page.ID, {}));
+        }
+    } else {
+        //Hint: don't forget to cover the else condition, because in our exercise hasVisibilityRules() is false
+        res.print(PageMgr.renderPage(page.ID, ""));
+    }
 
     next();
-
 });
 
 module.exports = server.exports();
