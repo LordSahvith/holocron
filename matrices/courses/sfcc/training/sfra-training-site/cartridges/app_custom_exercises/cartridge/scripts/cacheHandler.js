6c74725ab74c7546b978e0b7a743041283ce081b
Caleb Anderson <canderson@cricut.com>
2021-12-02 15:25:25 -0700

    9-2 partial page caching

--- matrices/courses/sfcc/training/sfra-training-site/cartridges/app_custom_exercises/cartridge/scripts/cacheHandler.js
+++ matrices/courses/sfcc/training/sfra-training-site/cartridges/app_custom_exercises/cartridge/scripts/cacheHandler.js
@@ -1,13 +1,11 @@
 'use strict';
 
-
 function setCache(req, res, next) {
-    res.cachePeriod = 24; // eslint-disable-line no-param-reassign
-    res.cachePeriodUnit = 'hours'; // eslint-disable-line no-param-reassign
+    res.cachePeriod = 2; // eslint-disable-line no-param-reassign
+    res.cachePeriodUnit = 'minutes'; // eslint-disable-line no-param-reassign
     next();
 }
 
 module.exports = {
-		setCache: setCache
-
+	setCache: setCache
 };
