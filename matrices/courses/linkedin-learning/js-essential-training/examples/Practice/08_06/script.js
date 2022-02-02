f398611460bb03876fdd3931ae06c77322537123
Caleb Anderson <43423985+canderson-cricut@users.noreply.github.com>
2022-02-01 18:29:13 -0700

    create front end files with js

--- matrices/courses/linkedin-learning/js-essential-training/examples/Practice/08_06/script.js
+++
@@ -1,7 +0,0 @@
-/**
- * Practice: Pass values between functions
- *
- * - Create two functions
- * - Main function creates article element with data from object
- * - Helper function creates.
- */
---
+++ matrices/courses/udemy/git-web-dev/node-expirements/create-frontend-files.js
@@ -0,0 +1,41 @@
+let fs = require('fs');
+
+let htmlFile = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="main.js" async></script>
+</head>
+<body>
+
+</body>
+</html>
+`;
+
+fs.writeFile(__dirname + '/index.html', htmlFile, function(error) {
+    if(error) {
+        console.log(error);
+    } else {
+        console.log('html file built.');
+    }
+});
+
+fs.writeFile(__dirname + '/style.scss', '', function(error) {
+    if(error) {
+        console.log(error);
+    } else {
+        console.log('sass file built.');
+    }
+});
+
+fs.writeFile(__dirname + '/main.js', '', function(error) {
+    if(error) {
+        console.log(error);
+    } else {
+        console.log('js file built.');
+    }
+});
