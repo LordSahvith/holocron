902a3c908450124a72304911a359dd64415ded51
Caleb Anderson <43423985+canderson-cricut@users.noreply.github.com>
2022-05-01 12:59:54 -0600

    practice 1

--- matrices/courses/linkedin-learning/js/js-essential-training/examples/Practice/03_07/script.js
+++ matrices/courses/linkedin-learning/js/js-essential-training/examples/Practice/03_07/script.js
@@ -7,3 +7,17 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const ACBrotherhoodBook = {
+    type: "paper back",
+    pageNum: 535
+};
+
+const bookshelf = {
+    shelfCount: 4,
+    books: {
+        ACBrotherhoodBook
+    }
+};
+
+console.log(bookshelf);
