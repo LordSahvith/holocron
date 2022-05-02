86a7605f46e10eefaba0a27f36f16333c42ccc90
Caleb Anderson <43423985+canderson-cricut@users.noreply.github.com>
2022-05-02 15:44:46 -0600

    remove first go

--- matrices/courses/linkedin-learning/js/js-essential-training/examples/Practice/05_04/script.js
+++ matrices/courses/linkedin-learning/js/js-essential-training/examples/Practice/05_04/script.js
@@ -3,38 +3,6 @@
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
 
-// let leftStrap = document.querySelector('[data-side="left"]');
-// let rightStrap = document.querySelector('[data-side="right"]');
-
-// let leftStrapLength = leftStrap.querySelector('span:first-child');
-// let rightStrapLength = rightStrap.querySelector('span:first-child');
-
-// let leftStrapInput = leftStrap.querySelector('.leftlength input');
-// let rightStrapInput = rightStrap.querySelector('.rightlength input');
-
-// let leftStrapButton = leftStrap.querySelector('.leftlength button');
-// let rightStrapButton = rightStrap.querySelector('.rightlength button');
-
-// let pack1OpenLid = document.querySelector('#pack01 .backpack__lid span');
-// let pack1OpenLidButton = document.querySelector('#pack01 .lid-toggle');
-
-// leftStrapButton.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     let newValue = leftStrapInput.value;
-//     leftStrapLength.innerHTML = `${newValue} inches`;
-// });
-
-// rightStrapButton.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     let newValue = rightStrapInput.value;
-//     rightStrapLength.innerHTML = `${newValue} inches`;
-// });
-
-// pack1OpenLidButton.addEventListener('click', function () {
-//     let pack1IsLidOpen = pack1OpenLid.innerHTML === 'closed' ? false : true;
-//     pack1OpenLid.innerHTML = pack1IsLidOpen ? 'closed' : 'open';
-// });
-
 let backpacks = document.querySelectorAll('.backpack');
 
 function addEventListeners(backpack) {
