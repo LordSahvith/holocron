8b83c826a990a153a00ed7e2e483f0b94ea5bfbc
Caleb Anderson <43423985+canderson-cricut@users.noreply.github.com>
2022-08-15 10:58:04 -0600

    Ch. 3.2 Destructuring Arrays

---
+++ matrices/courses/linkedin-learning/js/ECMAScript-6/my_work/03_arrays_and_methods/02_destructuring_arrays/script.js
@@ -0,0 +1,14 @@
+let cities = ['LA', 'NY', 'Kass City'];
+
+console.log(cities);
+
+let [first, two, bob] = ['LA', 'NY', 'Kass City'];
+
+console.log(first);
+console.log(bob);
+console.log(two);
+
+let [twenty,, bobby] = ['LA', 'NY', 'Kass City'];
+
+console.log(twenty);
+console.log(bobby);
