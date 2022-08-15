a064f0f15fbe5b6a8a40bf842e7801d5735e4eff
Caleb Anderson <43423985+canderson-cricut@users.noreply.github.com>
2022-08-15 11:00:18 -0600

    Ch. 3.3 Searching Arrays

---
+++ matrices/courses/linkedin-learning/js/ECMAScript-6/my_work/03_arrays_and_methods/03_search_arrays/script.js
@@ -0,0 +1,5 @@
+let cities = ['LA', 'NY', 'Kass City'];
+
+console.log(cities.includes('LA')); // true
+console.log(cities.includes('La')); // false
+console.log(cities.includes('Death Valley')); // false
