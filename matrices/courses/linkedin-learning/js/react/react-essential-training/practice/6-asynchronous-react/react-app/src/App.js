4f370d641b85375364fb5b5164ab0fb9f3fcc82c
Caleb Anderson <43423985+canderson-cricut@users.noreply.github.com>
2022-09-12 15:08:19 -0600

    Ch 6.2 - displaying data from an API

--- matrices/courses/linkedin-learning/js/react/react-essential-training/practice/6-asynchronous-react/react-app/src/App.js
+++ matrices/courses/linkedin-learning/js/react/react-essential-training/practice/6-asynchronous-react/react-app/src/App.js
@@ -1,17 +1,29 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 
+function GithubUser({ name, avatar }) {
+    return (
+        <div>
+            <h1>{name}</h1>
+            <img src={avatar} height="150" alt={name} />
+        </div>
+    );
+}
+
 function App() {
     const [data, setData] = useState(null);
     useEffect(() => {
         fetch('https://api.github.com/users/LordSahvith')
-        .then((response) => response.json())
-        .then(setData);
+            .then((response) => response.json())
+            .then(setData);
     }, []);
 
     if (data) {
         return (
-            <pre>{JSON.stringify(data, null, 2)}</pre>
+            <GithubUser
+                name={data.login}
+                avatar={data.avatar_url}
+            />
         )
     }
 
