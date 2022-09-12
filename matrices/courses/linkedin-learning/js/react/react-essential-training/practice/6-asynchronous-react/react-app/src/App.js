f927aaca1a828e3a6d61fc30db79df1598bcc38e
Caleb Anderson <43423985+canderson-cricut@users.noreply.github.com>
2022-09-12 15:18:12 -0600

    Ch 6.3 - handling loading states

--- matrices/courses/linkedin-learning/js/react/react-essential-training/practice/6-asynchronous-react/react-app/src/App.js
+++ matrices/courses/linkedin-learning/js/react/react-essential-training/practice/6-asynchronous-react/react-app/src/App.js
@@ -12,22 +12,28 @@ function GithubUser({ name, avatar }) {
 
 function App() {
     const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
     useEffect(() => {
+        setLoading(true);
         fetch('https://api.github.com/users/LordSahvith')
             .then((response) => response.json())
-            .then(setData);
+            .then(setData)
+            .then(() => setLoading(false))
+            .catch(setError);
     }, []);
 
-    if (data) {
-        return (
-            <GithubUser
-                name={data.login}
-                avatar={data.avatar_url}
-            />
-        )
-    }
+    if (loading) return <h1>Loading...</h1>
+    if (error) return <pre>{JSON.stringify(error)}</pre>
+    if (!data) return null;
 
-    return <h1>Data</h1>;
+    return (
+        <GithubUser
+            name={data.login}
+            avatar={data.avatar_url}
+        />
+    );
 }
 
 export default App;
