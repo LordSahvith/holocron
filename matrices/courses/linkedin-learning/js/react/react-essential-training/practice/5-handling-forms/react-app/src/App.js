d5bd9ea06741ede2d8ed2842ac4058bde61031a2
Caleb Anderson <43423985+canderson-cricut@users.noreply.github.com>
2022-09-12 13:08:35 -0600

    Ch 5.1 - working with uncontrolled components

--- matrices/courses/linkedin-learning/js/react/react-essential-training/practice/5-handling-forms/react-app/src/App.js
+++ matrices/courses/linkedin-learning/js/react/react-essential-training/practice/5-handling-forms/react-app/src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import { useRef } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const txtTitle = useRef();
+    const hexColor = useRef();
+
+    console.log(txtTitle);
+
+    const submit = (e) => {
+        e.preventDefault();
+        const title = txtTitle.current.value;
+        const color = hexColor.current.value;
+        alert(`${title}, ${color}`);
+        txtTitle.current.value = '';
+        hexColor.current.value = '';
+    };
+
+    return (
+        <div className="App">
+            <form onSubmit={submit}>
+                <input ref={txtTitle} type="text" placeholder="color title..." />
+                <input ref={hexColor} type="color" />
+                <button>ADD</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
