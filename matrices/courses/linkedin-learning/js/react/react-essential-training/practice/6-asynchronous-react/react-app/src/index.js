0b6b15918354443382b196082e0b066fd07baf06
Lord Savith <bulletproofchild@gmail.com>
2022-10-06 16:34:30 -0600

    ch 6 asynchronous react - workingwith render props; add new js to index.js

--- matrices/courses/linkedin-learning/js/react/react-essential-training/practice/6-asynchronous-react/react-app/src/index.js
+++ matrices/courses/linkedin-learning/js/react/react-essential-training/practice/6-asynchronous-react/react-app/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import RenderProps from './RenderProps';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <RenderProps />
     <App />
   </React.StrictMode>
 );
