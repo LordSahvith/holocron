efd3228b3125dbfa1f37524699dc9999504e9e26
Lord Savith <bulletproofchild@gmail.com>
2022-10-05 20:03:55 -0600

    ch 4 - add back useState & useEffect examples

--- matrices/courses/linkedin-learning/js/react/react-essential-training/practice/4-state-in-component-tree/react-app/src/App.js
+++ matrices/courses/linkedin-learning/js/react/react-essential-training/practice/4-state-in-component-tree/react-app/src/App.js
@@ -1,18 +1,43 @@
 import './App.css';
-import {useReducer} from 'react';
+import { useState, useEffect } from 'react';
+import { useReducer } from 'react';
 
 function App() {
-    const [checked, setChecked] = useReducer(
-        (checked) => !checked,
-        false
-    );
+    const [emotion, setEmotion] = useState('happy');
+    const [secondary, setSecondary] = useState('tired');
 
-  return (
-    <div className="App">
-        <input id="check" type="checkbox" value={checked} onChange={setChecked} />
-        <label htmlFor="check">{checked ? "checked" : "not checked"}</label>
-    </div>
-  );
+    useEffect(() => {
+        console.log(`It's ${emotion} right now`);
+    }, [emotion]); // dependency array defines when this should run
+
+    useEffect(() => {
+        console.log(`It's ${secondary} right now`);
+    }, [secondary]);
+
+    const [checked, setChecked] = useReducer((checked) => !checked, false);
+
+    return (
+        <div className="App">
+            <h1>Current emotion is {emotion}</h1>
+            <button onClick={() => setEmotion('sad')}>Sad</button>
+            <button onClick={() => setEmotion('excited')}>Excited</button>
+
+            <h2>Current secondary emotion is {secondary}.</h2>
+            <button onClick={() => setSecondary('grateful')}>Grateful</button>
+
+            <div>
+                <input
+                    id="check"
+                    type="checkbox"
+                    value={checked}
+                    onChange={setChecked}
+                />
+                <label htmlFor="check">
+                    {checked ? 'checked' : 'not checked'}
+                </label>
+            </div>
+        </div>
+    );
 }
 
 export default App;
