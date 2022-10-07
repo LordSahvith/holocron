902e4e3c1a7020f03af1bd583134806394ffcd89
Lord Savith <bulletproofchild@gmail.com>
2022-10-07 12:19:12 -0600

    ch 3 node globals - incorporate setInterval (asyncronous)

--- matrices/courses/linkedin-learning/js/node/node-essential-training/practice/3-globals/timers.js
+++ matrices/courses/linkedin-learning/js/node/node-essential-training/practice/3-globals/timers.js
@@ -1,6 +1,18 @@
 const waitTime = 3000;
+const waitInterval = 500;
+let currentTime = 0;
+
+const incrementedTime = () => {
+    currentTime += waitInterval;
+    console.log(`waiting ${currentTime / 1000} seconds`);
+};
+
 console.log(`setting a ${waitTime / 1000} second delay`);
 
-const timerFinished = () => console.log('done');
+const timerFinished = () => {
+    clearInterval(interval);
+    console.log('done');
+};
 
+const interval = setInterval(incrementedTime, waitInterval);
 setTimeout(timerFinished, waitTime);
