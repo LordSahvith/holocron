2c97800947e0f64b98a1385ad8c15be9b5d3a516
Lord Savith <bulletproofchild@gmail.com>
2022-10-07 12:24:11 -0600

    ch 3 node globals - report progrress with setInterval (asyncronous)

--- matrices/courses/linkedin-learning/js/node/node-essential-training/practice/3-globals/timers.js
+++ matrices/courses/linkedin-learning/js/node/node-essential-training/practice/3-globals/timers.js
@@ -1,16 +1,21 @@
-const waitTime = 3000;
+const waitTime = 5000;
 const waitInterval = 500;
 let currentTime = 0;
 
 const incrementedTime = () => {
     currentTime += waitInterval;
-    console.log(`waiting ${currentTime / 1000} seconds`);
+    const timePercentage = Math.floor((currentTime / waitTime) * 100);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    console.log(`waiting ... ${timePercentage}%`);
 };
 
 console.log(`setting a ${waitTime / 1000} second delay`);
 
 const timerFinished = () => {
     clearInterval(interval);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
     console.log('done');
 };
 
