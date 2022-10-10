24221ccae350f305adf442f4877f230a95cc2b18
Lord Savith <bulletproofchild@gmail.com>
2022-10-10 11:21:35 -0600

    ch 4 Node Modules - improve a module with EventEmitter

--- matrices/courses/linkedin-learning/js/node/node-essential-training/practice/4-node-modules/questions.js
+++ matrices/courses/linkedin-learning/js/node/node-essential-training/practice/4-node-modules/questions.js
@@ -3,11 +3,18 @@ const collectAnswers = require('./lib/collectAnswers');
 const questions = [
     'What is your name? ',
     'What is your powwer level? ',
-    'What are you going to do? '
+    'What are you going to do? ',
 ];
 
-collectAnswers(questions, answers => {
+const answerEvents = collectAnswers(questions);
+
+answerEvents.on('answer', (answer) => {
+    console.log(`question answered: ${answer}`);
+});
+
+answerEvents.on('complete', (answers) => {
     console.log('Thank you for your answers. ');
     console.log(answers);
-    process.exit();
 });
+
+answerEvents.on('complete', () => process.exit());
