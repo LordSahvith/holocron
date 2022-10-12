1db223b1f09b97a83dd74562c0d3a9473be00127
Lord Savith <bulletproofchild@gmail.com>
2022-10-12 12:56:39 -0600

    ch 6 Files and Streams - readable file streams

--- matrices/courses/linkedin-learning/js/node/node-essential-training/practice/6-files-and-streams/readStream.js
+++ matrices/courses/linkedin-learning/js/node/node-essential-training/practice/6-files-and-streams/readStream.js
@@ -1,4 +1,25 @@
-console.log("type something...");
-process.stdin.on("data", data => {
-  console.log(`I read ${data.length - 1} characters of text`);
+// console.log("type something...");
+// process.stdin.on("data", data => {
+//   console.log(`I read ${data.length - 1} characters of text`);
+// });
+
+const fs = require('fs');
+const readStream = fs.createReadStream('./assets/lorum-ipsum.md', 'utf-8');
+
+let fileText = '';
+
+console.log('type something...');
+
+readStream.on('data', (data) => {
+    console.log(`I read ${data.length -1} characters of text.`);
+    fileText += data;
 });
+
+readStream.once('data', (data) => {
+    console.log(data);
+});
+
+readStream.on('end', () => {
+    console.log('finished reading file');
+    console.log(`In total I read ${fileText.length - 1} characters of text.`);
+})
