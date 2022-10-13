e60d3e0faf0b826a658b3b0b569e810c77d4234e
Lord Savith <bulletproofchild@gmail.com>
2022-10-13 14:34:47 -0600

    ch3 Fundamental Concepts - inserting rows

--- matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch3-fundamental-concepts.sql
+++ matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch3-fundamental-concepts.sql
@@ -10,3 +10,24 @@ INSERT INTO test VALUES ( 1, 'a' );
 INSERT INTO test VALUES ( 2, 'b' );
 INSERT INTO test VALUES ( 3, 'c' );
 SELECT * FROM test;
+
+-- 02 DROP TABLE
+-- test.db
+
+CREATE TABLE test ( a TEXT, b TEXT );
+INSERT INTO test VALUES ( 'one', 'two' );
+SELECT * FROM test;
+DROP TABLE test;
+DROP TABLE IF EXISTS test;
+
+-- 03 INSERT INTO
+-- test.db
+
+CREATE TABLE test ( a INTEGER, b TEXT, c TEXT );
+
+INSERT INTO test VALUES ( 1, 'This', 'Right here!' ); 
+INSERT INTO test ( b, c ) VALUES ( 'That', 'Over there!' ); 
+INSERT INTO test DEFAULT VALUES;
+INSERT INTO test ( a, b, c ) SELECT id, name, description from item;
+
+SELECT * FROM test;
