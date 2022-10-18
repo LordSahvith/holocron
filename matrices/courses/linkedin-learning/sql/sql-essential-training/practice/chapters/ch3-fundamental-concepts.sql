3cc2ae7a5344ad127c518ebdf345d5b58957e2c3
Lord Savith <bulletproofchild@gmail.com>
2022-10-18 10:17:24 -0600

    ch 3 Fundamental Concepts - conditional expressions

--- matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch3-fundamental-concepts.sql
+++ matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch3-fundamental-concepts.sql
@@ -164,3 +164,25 @@ SELECT Name FROM Country ORDER BY Name DESC;
 SELECT Name FROM Country ORDER BY Name ASC;
 SELECT Name, Continent FROM Country ORDER BY Continent, Name;
 SELECT Name, Continent, Region FROM Country ORDER BY Continent DESC, Region, Name;
+
+-- 12 CASE
+-- test.db
+
+DROP TABLE IF EXISTS booltest;
+CREATE TABLE booltest (a INTEGER, b INTEGER);
+INSERT INTO booltest VALUES (1, 0);
+SELECT * FROM booltest;
+
+SELECT
+    CASE WHEN a THEN 'true' ELSE 'false' END as boolA,
+    CASE WHEN b THEN 'true' ELSE 'false' END as boolB
+    FROM booltest
+;
+
+SELECT
+  CASE a WHEN 1 THEN 'true' ELSE 'false' END AS boolA,
+  CASE b WHEN 1 THEN 'true' ELSE 'false' END AS boolB 
+  FROM booltest
+;
+
+DROP TABLE IF EXISTS booltest;
