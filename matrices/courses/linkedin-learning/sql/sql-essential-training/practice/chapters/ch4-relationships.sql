a3e46c0451256dd01c759227216a3a48e7296720
Lord Savith <bulletproofchild@gmail.com>
2022-10-18 10:48:29 -0600

    ch 4 Relationships - relating multiple tables

--- matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch4-relationships.sql
+++ matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch4-relationships.sql
@@ -50,4 +50,32 @@ SELECT s.id AS sale, s.date, i.name, i.description, s.price
   FROM sale AS s
   JOIN item AS i ON s.item_id = i.id
   ;
-  
+
+-- 03 Junction Table
+-- test.db
+
+SELECT * FROM customer;
+SELECT * FROM item;
+SELECT * FROM sale;
+
+SELECT c.name AS Cust, c.zip, i.name AS Item, i.description, s.quantity AS Quan, s.price AS Price
+  FROM sale AS s
+  JOIN item AS i ON s.item_id = i.id
+  JOIN customer AS c ON s.customer_id = c.id
+  ORDER BY Cust, Item
+;
+
+-- a customer without sales
+INSERT INTO customer ( name ) VALUES ( 'Jane Smith' );
+SELECT * FROM customer;
+
+-- left joins
+SELECT c.name AS Cust, c.zip, i.name AS Item, i.description, s.quantity AS Quan, s.price AS Price
+  FROM customer AS c
+  LEFT JOIN sale AS s ON s.customer_id = c.id
+  LEFT JOIN item AS i ON s.item_id = i.id
+  ORDER BY Cust, Item
+;
+
+-- restore database
+DELETE FROM customer WHERE id = 4;
