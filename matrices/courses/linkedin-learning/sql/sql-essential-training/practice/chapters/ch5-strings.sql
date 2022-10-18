80ae2eab0c6707990bd8632e68396763d32524dc
Lord Savith <bulletproofchild@gmail.com>
2022-10-18 11:11:36 -0600

    ch 5 Strings - folding case

--- matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch5-strings.sql
+++ matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch5-strings.sql
@@ -23,3 +23,12 @@ SELECT TRIM('   string   ');
 SELECT LTRIM('   string   ');
 SELECT RTRIM('   string   ');
 SELECT TRIM('...string...', '.');
+
+-- 05 UPPER/LOWER
+-- world.db
+
+SELECT 'StRiNg' = 'string';
+SELECT LOWER('StRiNg') = LOWER('string');
+SELECT UPPER('StRiNg') = UPPER('string');
+SELECT UPPER(Name) FROM City ORDER BY Name;
+SELECT LOWER(Name) FROM City ORDER BY Name;
