708192779d3148fc2cb17529500a8c47904d832b
Lord Savith <bulletproofchild@gmail.com>
2022-10-18 09:45:50 -0600

    ch 3 Fundamental Concepts - the NULL value

--- matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch3-fundamental-concepts.sql
+++ matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch3-fundamental-concepts.sql
@@ -39,3 +39,31 @@ SELECT * FROM test;
 DELETE FROM test WHERE a = 3;
 SELECT * FROM test WHERE a = 1;
 DELETE FROM test WHERE a = 1;
+
+-- 05 NULL
+-- test.db
+
+SELECT * FROM test;
+SELECT * FROM test WHERE a = NULL;
+SELECT * FROM test WHERE a IS NULL;
+SELECT * FROM test WHERE a IS NOT NULL;
+INSERT INTO test ( a, b, c ) VALUES ( 0, NULL, '' );
+SELECT * FROM test WHERE b IS NULL;
+SELECT * FROM test WHERE b = '';
+SELECT * FROM test WHERE c = '';
+SELECT * FROM test WHERE c IS NULL;
+
+DROP TABLE IF EXISTS test;
+CREATE TABLE test (
+  a INTEGER NOT NULL,
+  b TEXT NOT NULL,
+  c TEXT
+);
+
+INSERT INTO test VALUES ( 1, 'this', 'that' );
+SELECT * FROM test;
+
+INSERT INTO test ( b, c ) VALUES ( 'one', 'two' );
+INSERT INTO test ( a, c ) VALUES ( 1, 'two' );
+INSERT INTO test ( a, b ) VALUES ( 1, 'two' );
+DROP TABLE IF EXISTS test;
