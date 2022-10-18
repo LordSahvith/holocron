e590d8928d5db86675d9de5e6dfbb273b41641f7
Lord Savith <bulletproofchild@gmail.com>
2022-10-18 10:06:06 -0600

    ch 3 Fundamental Concepts - changing a schema

--- matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch3-fundamental-concepts.sql
+++ matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch3-fundamental-concepts.sql
@@ -80,3 +80,18 @@ CREATE TABLE test ( a TEXT, b TEXT, c TEXT NOT NULL );
 CREATE TABLE test ( a TEXT, b TEXT, c TEXT DEFAULT 'panda' );
 CREATE TABLE test ( a TEXT UNIQUE, b TEXT, c TEXT DEFAULT 'panda' );
 CREATE TABLE test ( a TEXT UNIQUE NOT NULL, b TEXT, c TEXT DEFAULT 'panda' );
+
+-- 07 ALTER TABLE
+-- test.db
+
+DROP TABLE IF EXISTS test;
+CREATE TABLE test ( a TEXT, b TEXT, c TEXT );
+INSERT INTO test VALUES ( 'one', 'two', 'three');
+INSERT INTO test VALUES ( 'two', 'three', 'four');
+INSERT INTO test VALUES ( 'three', 'four', 'five');
+SELECT * FROM test;
+
+ALTER TABLE test ADD d TEXT;
+ALTER TABLE test ADD e TEXT DEFAULT 'panda';
+
+DROP TABLE IF EXISTS test;
