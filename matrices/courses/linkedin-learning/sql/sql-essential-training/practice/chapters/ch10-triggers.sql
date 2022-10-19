828a6562fab2308734ce00589fba4958faff73cd
Lord Savith <bulletproofchild@gmail.com>
2022-10-19 11:46:11 -0600

    ch 10 Triggers - preventing updates

--- matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch10-triggers.sql
+++ matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch10-triggers.sql
@@ -21,3 +21,28 @@ INSERT INTO widgetSale (item_id, customer_id, quan, price) VALUES (2, 2, 3, 1495
 INSERT INTO widgetSale (item_id, customer_id, quan, price) VALUES (3, 1, 1, 2995);
 SELECT * FROM widgetSale;
 SELECT * FROM widgetCustomer;
+
+-- 02 preventing updates
+-- test.db
+
+DROP TABLE IF EXISTS widgetSale;
+
+CREATE TABLE widgetSale ( id integer primary key, item_id INT, customer_id INTEGER, quan INT, price INT,
+    reconciled INT );
+INSERT INTO widgetSale (item_id, customer_id, quan, price, reconciled) VALUES (1, 3, 5, 1995, 0);
+INSERT INTO widgetSale (item_id, customer_id, quan, price, reconciled) VALUES (2, 2, 3, 1495, 1);
+INSERT INTO widgetSale (item_id, customer_id, quan, price, reconciled) VALUES (3, 1, 1, 2995, 0);
+SELECT * FROM widgetSale;
+
+CREATE TRIGGER updateWidgetSale BEFORE UPDATE ON widgetSale
+    BEGIN
+        SELECT RAISE(ROLLBACK, 'cannot update table "widgetSale"') FROM widgetSale
+            WHERE id = NEW.id AND reconciled = 1;
+    END
+;
+
+BEGIN TRANSACTION;
+UPDATE widgetSale SET quan = 9 WHERE id = 2;
+END TRANSACTION;
+
+SELECT * FROM widgetSale;
