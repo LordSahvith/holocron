e50970f82abea5d0dfe1c2c9921be388f0d5b2b9
Lord Savith <bulletproofchild@gmail.com>
2022-10-19 11:58:00 -0600

    ch 11 Views and Subselects - searching within a result set

--- matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch11-views-subselects.sql
+++ matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch11-views-subselects.sql
@@ -19,3 +19,31 @@ SELECT co.Name, ss.CCode FROM (
 ;
 
 DROP TABLE t;
+
+-- 02 searching within a result set
+-- album.db
+
+SELECT DISTINCT album_id FROM track WHERE duration <= 90;
+
+SELECT * FROM album
+  WHERE id IN (SELECT DISTINCT album_id FROM track WHERE duration <= 90)
+;
+
+SELECT a.title AS album, a.artist, t.track_number AS seq, t.title, t.duration AS secs
+  FROM album AS a
+  JOIN track AS t
+    ON t.album_id = a.id
+  WHERE a.id IN (SELECT DISTINCT album_id FROM track WHERE duration <= 90)
+  ORDER BY a.title, t.track_number
+;
+
+SELECT a.title AS album, a.artist, t.track_number AS seq, t.title, t.duration AS secs
+  FROM album AS a
+  JOIN (
+    SELECT DISTINCT album_id, track_number, duration, title
+      FROM track
+      WHERE duration <= 90
+  ) AS t
+    ON t.album_id = a.id
+  ORDER BY a.title, t.track_number
+;
