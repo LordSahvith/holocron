7261d092f9f0c14bf7ea9129c5398ca0d3aebb01
Lord Savith <bulletproofchild@gmail.com>
2022-10-19 12:01:48 -0600

    ch 11 Views and Subselects - creating a joined view

--- matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch11-views-subselects.sql
+++ matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch11-views-subselects.sql
@@ -67,3 +67,38 @@ SELECT a.title AS album, a.artist, t.track_number AS seq, t.title, t.m, t.s
 ;
 
 DROP VIEW IF EXISTS trackView;
+
+-- 04 Joined view
+-- album.db
+
+SELECT a.artist AS artist,
+    a.title AS album,
+    t.title AS track,
+    t.track_number AS trackno,
+    t.duration / 60 AS m,
+    t.duration % 60 AS s
+  FROM track AS t
+    JOIN album AS a
+      ON a.id = t.album_id
+;
+
+CREATE VIEW joinedAlbum AS
+  SELECT a.artist AS artist,
+      a.title AS album,
+      t.title AS track,
+      t.track_number AS trackno,
+      t.duration / 60 AS m,
+      t.duration % 60 AS s
+    FROM track AS t
+    JOIN album AS a
+      ON a.id = t.album_id
+;
+
+SELECT * FROM joinedAlbum;
+SELECT * FROM joinedAlbum WHERE artist = 'Jimi Hendrix';
+
+SELECT artist, album, track, trackno, 
+   m || ':' || substr('00' || s, -2, 2) AS duration
+    FROM joinedAlbum;
+
+DROP VIEW IF EXISTS joinedAlbum;
