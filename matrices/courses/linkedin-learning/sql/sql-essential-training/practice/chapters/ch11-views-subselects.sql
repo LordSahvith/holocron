ac3fa15653e8e77f9416daa7e0148951afe9313e
Lord Savith <bulletproofchild@gmail.com>
2022-10-19 11:59:55 -0600

    ch 11 Views and Subselects - creating a view

--- matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch11-views-subselects.sql
+++ matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch11-views-subselects.sql
@@ -47,3 +47,23 @@ SELECT a.title AS album, a.artist, t.track_number AS seq, t.title, t.duration AS
     ON t.album_id = a.id
   ORDER BY a.title, t.track_number
 ;
+
+-- 03 Creating a view
+-- album.db
+
+SELECT id, album_id, title, track_number, 
+  duration / 60 AS m, duration % 60 AS s FROM track;
+
+CREATE VIEW trackView AS
+  SELECT id, album_id, title, track_number, 
+    duration / 60 AS m, duration % 60 AS s FROM track;
+SELECT * FROM trackView;
+
+SELECT a.title AS album, a.artist, t.track_number AS seq, t.title, t.m, t.s
+  FROM album AS a
+  JOIN trackView AS t
+    ON t.album_id = a.id
+  ORDER BY a.title, t.track_number
+;
+
+DROP VIEW IF EXISTS trackView;
