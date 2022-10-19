dcfa083b4449ab2fb7b52d1f8c062bdee3adbeb1
Lord Savith <bulletproofchild@gmail.com>
2022-10-19 11:37:50 -0600

    ch 9 Transactions - performance

--- matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch9-transactions.sql
+++ matrices/courses/linkedin-learning/sql/sql-essential-training/practice/chapters/ch9-transactions.sql
@@ -34,3 +34,22 @@ SELECT * FROM widgetInventory;
 -- restore database
 DROP TABLE IF EXISTS widgetInventory;
 DROP TABLE IF EXISTS widgetSales;
+
+-- 03 performance
+-- test.db
+
+CREATE TABLE test ( id INTEGER PRIMARY KEY, data TEXT );
+
+-- put this before the 1,000 INSERT statements
+BEGIN TRANSACTION;
+
+-- copy / paste 1,000 of these ...
+INSERT INTO test ( data ) VALUES ( 'this is a good sized line of text.' );
+
+-- put this after the 1,000 INSERT statements
+END TRANSACTION;
+
+SELECT COUNT(*) FROM test;
+
+-- restore database
+DROP TABLE test;
