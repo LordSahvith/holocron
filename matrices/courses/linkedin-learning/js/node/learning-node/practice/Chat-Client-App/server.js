19fa037240b660b5a5970b4fe852d6bfd22c7483
Lord Savith <bulletproofchild@gmail.com>
2022-10-20 11:52:13 -0600

    ch 5 Chat Client App - create a post messages service

--- matrices/courses/linkedin-learning/js/node/learning-node/practice/Chat-Client-App/server.js
+++ matrices/courses/linkedin-learning/js/node/learning-node/practice/Chat-Client-App/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 const messages = [
     {name: 'Bob', message: 'Why?'},
@@ -10,6 +13,12 @@ const messages = [
 
 app.get('/messages', (req, res) => {
     res.send(messages);
+});
+
+app.post('/messages', (req, res) => {
+    console.log(req.body);
+    messages.push(req.body);
+    res.sendStatus(200);
 });
 
 const server = app.listen(3000, () => {
