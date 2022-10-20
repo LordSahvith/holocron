c5b6408f31b1a3d92558e3b5138559ca80602609
Lord Savith <bulletproofchild@gmail.com>
2022-10-20 11:18:32 -0600

    ch 5 Chat Client App - create a get message service

--- matrices/courses/linkedin-learning/js/node/learning-node/practice/Chat-Client-App/server.js
+++ matrices/courses/linkedin-learning/js/node/learning-node/practice/Chat-Client-App/server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname));
+
+const messages = [
+    {name: 'Bob', message: 'Why?'},
+    {name: 'Jane', message: 'Because?'}
+];
+
+app.get('/messages', (req, res) => {
+    res.send(messages);
+});
+
 const server = app.listen(3000, () => {
     console.log(`server is listening on port ${server.address().port}`);
 });
