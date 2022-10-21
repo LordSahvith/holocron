a538c52656c07891babec0155ee7d93602841a10
Lord Savith <bulletproofchild@gmail.com>
2022-10-21 10:34:08 -0600

    ch 7 Improving Asynchronous Code - nested callbacks; promises

--- matrices/courses/linkedin-learning/js/node/learning-node/practice/Chat-Client-App/server.js
+++ matrices/courses/linkedin-learning/js/node/learning-node/practice/Chat-Client-App/server.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const dbURL = 'mongodb+srv://lordsavith:Darthbane4@learning-node.dxjvxu6.mongodb.net/test';
 
@@ -25,11 +25,23 @@ app.get('/messages', (req, res) => {
 app.post('/messages', (req, res) => {
     const message = new Message(req.body);
 
-    message.save((error) => {
-        if (error) sendStatus(500);
- 
+    message.save()
+    .then(() => {
+        console.log('saved');
+        return Message.findOne({ message: 'badword' });
+    })
+    .then(censored => {
+        if (censored) {
+            console.log('censored words found:', censored);
+            return Message.deleteOne({ _id: censored.id });
+        }
+
         io.emit('message', req.body);
         res.sendStatus(200);
+    })
+    .catch((error) => {
+        res.sendStatus(500);
+        return console.log(error);
     });
 });
 
