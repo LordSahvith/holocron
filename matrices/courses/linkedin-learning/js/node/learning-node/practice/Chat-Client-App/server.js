c8e2e76ef594e3c31c4f711f1ebf7c74de73ebd3
Lord Savith <bulletproofchild@gmail.com>
2022-10-21 09:58:01 -0600

    ch 6 Exploring Databases - set up mongoDB; mongoose; saving data to mongoDB with mongoose

--- matrices/courses/linkedin-learning/js/node/learning-node/practice/Chat-Client-App/server.js
+++ matrices/courses/linkedin-learning/js/node/learning-node/practice/Chat-Client-App/server.js
@@ -3,28 +3,42 @@ const bodyParser = require('body-parser');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-const messages = [
-    {name: 'Bob', message: 'Why?'},
-    {name: 'Jane', message: 'Because?'}
-];
+const dbURL = 'mongodb+srv://lordsavith:Darthbane4@learning-node.dxjvxu6.mongodb.net/test';
+
+const Message = mongoose.model('Message', {
+    name: String,
+    message: String
+});
 
 app.get('/messages', (req, res) => {
-    res.send(messages);
+    Message.find({}, (error, messages) => {
+        res.send(messages);
+    });
 });
 
 app.post('/messages', (req, res) => {
-    messages.push(req.body);
-    io.emit('message', req.body);
-    res.sendStatus(200);
+    const message = new Message(req.body);
+
+    message.save((error) => {
+        if (error) sendStatus(500);
+ 
+        io.emit('message', req.body);
+        res.sendStatus(200);
+    });
 });
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+});
+
+mongoose.connect(dbURL, (error) => {
+    console.log('mongoDB connection:', error);
 });
 
 const server = http.listen(3000, () => {
