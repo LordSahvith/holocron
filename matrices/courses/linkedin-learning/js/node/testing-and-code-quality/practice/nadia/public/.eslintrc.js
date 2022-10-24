be579a3d58c7a0550e8aa0483df418261a7dcf8a
Lord Savith <bulletproofchild@gmail.com>
2022-10-24 15:37:49 -0600

    ch 2 Finding Errors with Linting - linting your codebase

---
+++ matrices/courses/linkedin-learning/js/node/testing-and-code-quality/practice/nadia/public/.eslintrc.js
@@ -0,0 +1,14 @@
+({
+    "parserOptoins": {
+        "sourceType": "script"
+    },
+    "env": {
+        "broswer": true,
+        "jquery": true,
+        "node": false
+    },
+    "rules": {
+        "no-var": "off",
+        "prefer-const": "off"
+    }
+});
