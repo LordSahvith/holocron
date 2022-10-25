a7b5b6c69ac744579aaa2d68e606a53c3d37a17c
Lord Savith <bulletproofchild@gmail.com>
2022-10-25 14:23:01 -0600

    initialize file

---
+++ matrices/courses/linkedin-learning/js/node/testing-and-code-quality/practice/nadia/routes/reservations.test.js
@@ -0,0 +1,24 @@
+const request = require('supertest');
+
+let app;
+
+const mockMorgan = jest.fn((req, res, next) => next());
+
+beforeAll(() => {
+    jest.mock('morgan', () => () => mockMorgan);
+    app = request(require('../app'));
+});
+
+afterAll(() => {
+    jest.unmock('morgan');
+});
+
+describe('GET', () => {
+    it('should return the reservations form', async () => {
+        const response = await app.get('./reservations')
+            .expect('Content-Type', /html/)
+            .expect(200);
+
+        expect(response.text).toContain('To make reservations please fill out the following form');
+    });
+});
