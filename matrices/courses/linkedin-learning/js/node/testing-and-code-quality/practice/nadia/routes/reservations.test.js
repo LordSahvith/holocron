77e20f09f3819391e0049a97d329b542c37853eb
Lord Savith <bulletproofchild@gmail.com>
2022-10-25 14:41:34 -0600

    ch 5 Reporting on Your Entire Codebase - form submission with SuperTest

--- matrices/courses/linkedin-learning/js/node/testing-and-code-quality/practice/nadia/routes/reservations.test.js
+++ matrices/courses/linkedin-learning/js/node/testing-and-code-quality/practice/nadia/routes/reservations.test.js
@@ -3,9 +3,13 @@ const request = require('supertest');
 let app;
 
 const mockMorgan = jest.fn((req, res, next) => next());
+const mockInsert = jest.fn().mockResolvedValue([1349]);
 
 beforeAll(() => {
     jest.mock('morgan', () => () => mockMorgan);
+    jest.mock('../lib/knex', () => () => ({
+        insert: mockInsert
+    }));
     app = request(require('../app'));
 });
 
@@ -20,5 +24,37 @@ describe('GET', () => {
             .expect(200);
 
         expect(response.text).toContain('To make reservations please fill out the following form');
+    });
+});
+
+describe('POST', () => {
+    it('should reject an invalid reservation request', async () => {
+        const response = await app.post('/reservations')
+            .type('form')
+            .send({
+                date: '2017/06/10',
+                time: '06:02 AM',
+                party: 'bananas',
+                name: 'familiy',
+                email: 'username@example.com'
+            });
+
+        expect(response.text).toContain('Sorry, there was a problem with your booking request.');
+        expect(response.status).toBe(400);
+    });
+
+    it('should accept a valid reservation request', async () => {
+        const response = await app.post('/reservations')
+            .type('form')
+            .send({
+                date: '2017/06/10',
+                time: '06:02 AM',
+                party: '4',
+                name: 'familiy',
+                email: 'username@example.com'
+            })
+            .expect(200);
+
+        expect(response.text).toContain('Thanks, your booking request #1349');
     });
 });
