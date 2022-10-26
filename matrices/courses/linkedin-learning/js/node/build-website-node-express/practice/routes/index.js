5d2faebc28a603225541f5ba033c8e0055b81644
Lord Savith <bulletproofchild@gmail.com>
2022-10-26 14:54:43 -0600

    ch5 Handling Errors Gracefully - populating the error page

--- matrices/courses/linkedin-learning/js/node/build-website-node-express/practice/routes/index.js
+++ matrices/courses/linkedin-learning/js/node/build-website-node-express/practice/routes/index.js
@@ -9,7 +9,6 @@ module.exports = (params) => {
     const { speakersService } = params;
 
     router.get('/', async (request, response, next) => {
-        return next(new Error('Async await error'));
         try {
             const artwork = await speakersService.getAllArtwork();
             const topSpeakers = await speakersService.getList();
