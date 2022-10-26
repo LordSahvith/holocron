05b083017adf49af9545014f9d898e90cc0b93bb
Lord Savith <bulletproofchild@gmail.com>
2022-10-26 10:25:48 -0600

    ch3 Express Routes annd Middleware - add business logic

--- matrices/courses/linkedin-learning/js/node/build-website-node-express/practice/routes/speakers.js
+++ matrices/courses/linkedin-learning/js/node/build-website-node-express/practice/routes/speakers.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
-module.exports = () => {
-    router.get('/', (request, response) => response.send('Speakers list'));
+module.exports = (params) => {
+    const { speakersService } = params;
+
+    router.get('/', async (request, response) => {
+        const speakers = await speakersService.getList();
+        return response.json(speakers);
+    });
 
     router.get('/:shortname', (request, response) => response.send(`Detail page of ${request.params.shortname}`));
 
--- matrices/courses/linkedin-learning/js/node/build-website-node-express/practice/server.js
+++ matrices/courses/linkedin-learning/js/node/build-website-node-express/practice/server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const path = require('path');
+
+const FeedbackService = require('./services/FeedbackService');
+const SpeakerService = require('./services/SpeakerService');
+
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakersService = new SpeakerService('./data/speakers.json');
+
 const routes = require('./routes');
 
 const app = express();
@@ -11,7 +18,10 @@ app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static(path.join(__dirname, './static')));
 
-app.use('/', routes());
+app.use('/', routes({
+    feedbackService,
+    speakersService
+}));
 
 app.listen(port, () => {
     console.log(`Express server listening on port ${port}.`);
