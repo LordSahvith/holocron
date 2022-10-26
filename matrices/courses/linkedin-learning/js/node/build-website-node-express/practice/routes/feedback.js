9bedc4764f60c54aadbd6b06f35cb45ea203cb1a
Lord Savith <bulletproofchild@gmail.com>
2022-10-26 15:58:13 -0600

    ch6 Handling Form Data - storing data

--- matrices/courses/linkedin-learning/js/node/build-website-node-express/practice/routes/feedback.js
+++ matrices/courses/linkedin-learning/js/node/build-website-node-express/practice/routes/feedback.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const { check, validationResult } = require('express-validator');
+
 const router = express.Router();
 
 module.exports = (params) => {
@@ -8,15 +10,62 @@ module.exports = (params) => {
     router.get('/', async (request, response, next) => {
         try {
             const feedback = await feedbackService.getList();
-            return response.render('layout', { pageTitle: 'Feedback', template: 'feedback', feedback });
+
+            const errors = request.session.feedback ? request.session.feedback.errors : false;
+            const successMessage = request.session.feedback ? request.session.feedback.message : false;
+            request.session.feedback = {};
+
+            return response.render(
+                'layout',
+                {
+                    pageTitle: 'Feedback',
+                    template: 'feedback',
+                    feedback,
+                    errors,
+                    successMessage
+                });
         } catch (error) {
             return next(error);
         }
     });
 
-    router.post('/', (request, response) => {
-        console.log(request.body);
-        return response.send('Feedback form posted')
+    router.post('/', [
+        check('name')
+            .trim()
+            .isLength({ min: 3 })
+            .escape()
+            .withMessage('A name is required'),
+        check('email')
+            .trim()
+            .isEmail()
+            .normalizeEmail()
+            .withMessage('A valid email is required'),
+        check('title')
+            .trim()
+            .isLength({ min: 3 })
+            .escape()
+            .withMessage('A title is required'),
+        check('message')
+            .trim()
+            .isLength({ min: 5 })
+            .escape()
+            .withMessage('A message is required')
+    ], async (request, response) => {
+        const errors = validationResult(request);
+
+        if (!errors.isEmpty()) {
+            request.session.feedback = {
+                errors: errors.array()
+            };
+            return response.redirect('/feedback');
+        }
+
+        const { name, email, title, message } = request.body;
+        await feedbackService.addEntry(name, email, title, message);
+        request.session.feedback = {
+            message: 'Thank you for your feedback!'
+        };
+        return response.redirect('/feedback');
     });
 
     return router;
