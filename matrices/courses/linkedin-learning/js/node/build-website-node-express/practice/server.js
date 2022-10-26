40ce2480afdd019f8b13ec3e55acbdd0f43f37cf
Lord Savith <bulletproofchild@gmail.com>
2022-10-26 08:56:59 -0600

    ch2 Template Engines - rendering the index page wwith EJS

--- matrices/courses/linkedin-learning/js/node/build-website-node-express/practice/server.js
+++ matrices/courses/linkedin-learning/js/node/build-website-node-express/practice/server.js
@@ -5,10 +5,13 @@ const app = express();
 
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
 app.use(express.static(path.join(__dirname, './static')));
 
 app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname, './static/index.html'));
+    response.render('pages/index', { pageTitle: "Welcome" });
 });
 
 app.get('/speakers', (request, response) => {
