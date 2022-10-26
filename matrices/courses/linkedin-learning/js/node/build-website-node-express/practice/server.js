83e8dc33264067926dca4b49bc3301e9c5e65405
Lord Savith <bulletproofchild@gmail.com>
2022-10-26 10:36:27 -0600

    ch3 Express Routes annd Middleware - add session management middleware

--- matrices/courses/linkedin-learning/js/node/build-website-node-express/practice/server.js
+++ matrices/courses/linkedin-learning/js/node/build-website-node-express/practice/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
 
 const FeedbackService = require('./services/FeedbackService');
 const SpeakerService = require('./services/SpeakerService');
@@ -12,6 +13,13 @@ const routes = require('./routes');
 const app = express();
 
 const port = 3000;
+
+app.set('trust proxy', 1);
+
+app.use(cookieSession({
+    name: 'session',
+    keys: ['123abc', '456efg']
+}));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
