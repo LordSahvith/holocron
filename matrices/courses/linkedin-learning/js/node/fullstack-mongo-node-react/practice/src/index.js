4efbbf5bbbc6e63d7f50d2c934932f63cecf934c
Lord Savith <bulletproofchild@gmail.com>
2022-11-03 17:53:52 -0600

    ch3 React Basics - React elements and JSX

---
+++ matrices/courses/linkedin-learning/js/node/fullstack-mongo-node-react/practice/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h3 className="text-center">Hello React from JSX!</h3>,
+    document.getElementById('root')
+);
