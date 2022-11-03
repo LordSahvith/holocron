442d0ba09abc27663fbf1caa4c79f76b89a620d6
Lord Savith <bulletproofchild@gmail.com>
2022-11-03 16:05:34 -0600

    code cleanup

--- matrices/courses/linkedin-learning/js/node/fullstack-mongo-node-react/practice/server.js
+++ matrices/courses/linkedin-learning/js/node/fullstack-mongo-node-react/practice/server.js
@@ -1,6 +1,5 @@
 import config from './config';
 import apiRouter from './api';
-import fs from 'fs';
 import express from 'express';
 
 const server = express();
@@ -10,13 +9,7 @@ server.get('/', (req, res) => {
 });
 
 server.use('/api', apiRouter);
-// short hand - middleware - not for production env
 server.use(express.static('public'));
-// server.get('/about.html', (req, res) => {
-//     fs.readFile('./about.html', (err, data) => {
-//         res.send(data.toString());
-//     });
-// });
 
 server.listen(config.port, () => {
     console.info(`Express is listening on port ${config.port}`);
