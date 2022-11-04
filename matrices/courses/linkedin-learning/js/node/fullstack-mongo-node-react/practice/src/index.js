fda756d2c3d4c169c4e0bd72dd258cef79898609
Lord Savith <bulletproofchild@gmail.com>
2022-11-04 10:49:58 -0600

    ch3 React Basics - react components

--- matrices/courses/linkedin-learning/js/node/fullstack-mongo-node-react/practice/src/index.js
+++ matrices/courses/linkedin-learning/js/node/fullstack-mongo-node-react/practice/src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+const App = (props) => {
+    return (
+        <h2 className="text-center">{props.headerMessage}</h2>
+    );
+};
+
+App.propTypes = {
+    headerMessage: PropTypes.string.isRequired
+};
+
+App.defaultProps = {
+    headerMessage: 'Default Prop'
+};
 
 ReactDOM.render(
-    <h3 className="text-center">Hello React from JSX!</h3>,
+    <App />,
     document.getElementById('root')
 );
