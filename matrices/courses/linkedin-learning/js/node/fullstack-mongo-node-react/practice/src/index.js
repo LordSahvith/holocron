5ba408fd6bc3cc5327530453fe561d9bc0b3f842
Lord Savith <bulletproofchild@gmail.com>
2022-11-04 11:07:54 -0600

    fix devtools error for strict mode

--- matrices/courses/linkedin-learning/js/node/fullstack-mongo-node-react/practice/src/index.js
+++ matrices/courses/linkedin-learning/js/node/fullstack-mongo-node-react/practice/src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 ReactDOM.render(
-    <App />,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
     document.getElementById('root')
 );
