01457ca1babd59fad45cddfcaaeca2b4728ab281
Lord Savith <bulletproofchild@gmail.com>
2022-11-05 10:26:02 -0600

    ch4 Working with Data - displaying a list of objects

--- matrices/courses/linkedin-learning/js/node/fullstack-mongo-node-react/practice/src/components/App.js
+++ matrices/courses/linkedin-learning/js/node/fullstack-mongo-node-react/practice/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from './Header';
+import ContestPreview from './ContestPreview';
 
 class App extends React.Component {
     state = {
@@ -20,7 +21,9 @@ class App extends React.Component {
             <div className='App'>
                 <Header message={this.state.pageHeader} />
                 <div>
-                    ...
+                    {this.props.contests.map(contest =>
+                        <ContestPreview {...contest} />
+                    )}
                 </div>
             </div>
         );
