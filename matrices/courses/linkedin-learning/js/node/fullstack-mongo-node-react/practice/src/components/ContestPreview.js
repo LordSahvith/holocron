e3c7438b20d64d58b1c48c3af661ccf82391c576
Lord Savith <bulletproofchild@gmail.com>
2022-11-05 10:46:57 -0600

    ch4 Working with Data - using sass with node

--- matrices/courses/linkedin-learning/js/node/fullstack-mongo-node-react/practice/src/components/ContestPreview.js
+++ matrices/courses/linkedin-learning/js/node/fullstack-mongo-node-react/practice/src/components/ContestPreview.js
@@ -3,10 +3,10 @@ import React from 'react';
 const ContestPreview = (contest) => {
     return (
         <div className='ContestPreview'>
-            <div>
+            <div className='category-name'>
                 {contest.categoryName}
             </div>
-            <div>
+            <div className='contest-name'>
                 {contest.contestName}
             </div>
         </div>
