e4f05ef3e404875d5b21d5856d4a42b00e8c3aeb
Lord Savith <bulletproofchild@gmail.com>
2022-11-05 13:31:38 -0600

    ch5 Rendering on the Server - fetching data from the server side

---
+++ matrices/courses/linkedin-learning/js/node/fullstack-mongo-node-react/practice/serverRender.js
@@ -0,0 +1,8 @@
+import config from './config';
+import axios from 'axios';
+
+axios.get(`${config.serverUrl}/api/contests`)
+    .then(res => {
+        console.log(res.data);
+    })
+    .catch(console.error);
