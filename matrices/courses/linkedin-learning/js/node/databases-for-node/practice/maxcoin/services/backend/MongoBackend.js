99e432c01c74d107f90ddc97239effb0b5dd3a34
Lord Savith <bulletproofchild@gmail.com>
2022-11-07 16:37:00 -0700

    ch3 Use Document Databases with Node - connect to mongoDB

--- matrices/courses/linkedin-learning/js/node/databases-for-node/practice/maxcoin/services/backend/MongoBackend.js
+++ matrices/courses/linkedin-learning/js/node/databases-for-node/practice/maxcoin/services/backend/MongoBackend.js
@@ -1,23 +1,61 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-empty-function */
+
+const { MongoClient } = require('mongodb');
+
 const CoinAPI = require('../CoinAPI');
 
 class MongoBackend {
 
     constructor() {
         this.coinAPI = new CoinAPI();
+        this.mongoUrl = 'mongodb://localhost:27017/maxcoin';
+        this.client = null;
+        this.collection = null;
     }
 
-    async connect() { }
+    async connect() {
+        const mongoClient = new MongoClient(this.mongoUrl, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
 
-    async disconnect() { }
+        this.client = await mongoClient.connect();
+        this.collection = this.client.db('maxcoin').collection('values');
+        return this.client;
+    }
+
+    async disconnect() {
+        if (this.client) {
+            return this.client.close;
+        } 
+        
+        return false;
+    }
 
     async insert() { }
 
     async getMax() { }
 
-    async max() { }
+    async max() {
+        console.info('Connecting to MongoDB...\r');
+        console.time('mongodb-connect');
+
+        const client = await this.connect();
+
+        if (client) {
+            console.info('Successfully connected to MongoDB\r');
+        } else {
+            throw new Error('Connection to MongoDB Failed\r');
+        }
+        console.timeEnd('mongodb-connect');
+        
+        console.info('Disonnecting from MongoDB...\r');
+        console.time('mongodb-disconnect');
+        await this.disconnect();
+        console.timeEnd('mongodb-disconnect');
+    }
 }
 
 module.exports = MongoBackend;
