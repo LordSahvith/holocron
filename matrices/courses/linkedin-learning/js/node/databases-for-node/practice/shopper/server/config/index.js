7518e94d8d7ff490089edf86b1088d67a03a0571
Lord Savith <bulletproofchild@gmail.com>
2022-11-08 15:35:01 -0700

    ch4 Use Key-Value Stores with Node - add redis sessions to project

--- matrices/courses/linkedin-learning/js/node/databases-for-node/practice/shopper/server/config/index.js
+++ matrices/courses/linkedin-learning/js/node/databases-for-node/practice/shopper/server/config/index.js
@@ -4,5 +4,9 @@ module.exports = {
     applicationName: pkg.name,
     mongoDB: {
         url: 'mongodb://localhost:27017/shopper'
+    },
+    redis: {
+        port: 7379,
+        client: null
     }
 };
