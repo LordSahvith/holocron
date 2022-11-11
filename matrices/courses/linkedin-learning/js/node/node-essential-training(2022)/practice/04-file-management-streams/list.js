449f455ef4f055a869b7f4ae65f039df3fbcde6b
Lord Savith <bulletproofchild@gmail.com>
2022-11-11 08:21:51 -0700

    ch4 File Management and Streams - reading files

--- matrices/courses/linkedin-learning/js/node/node-essential-training(2022)/practice/04-file-management-streams/list.js
+++ matrices/courses/linkedin-learning/js/node/node-essential-training(2022)/practice/04-file-management-streams/list.js
@@ -7,4 +7,8 @@ fs.readdir("./", function (err, files) {
     console.log(files);
 });
 
+fs.readFile("./readme.md", "UTF-8", (err, ipsum) => {
+    console.log(ipsum);
+});
+
 console.log("reading files...");
