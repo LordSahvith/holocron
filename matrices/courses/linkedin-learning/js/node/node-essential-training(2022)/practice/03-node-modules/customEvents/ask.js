67f50d42cd6f9428d986f8169856a771bc58c619
Lord Savith <bulletproofchild@gmail.com>
2022-11-11 08:14:27 -0700

    ch3 Node Modules - custom events with the EventEmitter (but not really this video)

---
+++ matrices/courses/linkedin-learning/js/node/node-essential-training(2022)/practice/03-node-modules/customEvents/ask.js
@@ -0,0 +1,13 @@
+const collectAnswers = require("./lib/collectAnswers");
+
+const questions = [
+    'What is your name? ',
+    'What is your powwer level? ',
+    'What are you going to do? ',
+];
+
+collectAnswers(questions, (answers) => {
+    console.log("Thank you for your answers!");
+    console.log(answers);
+    process.exit();
+});
