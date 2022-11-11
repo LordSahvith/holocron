e18571e1b5b48efaa2c3c7bc9680ab5d5e6fc173
Lord Savith <bulletproofchild@gmail.com>
2022-11-11 07:17:51 -0700

    ch2 Node Globals - incorporating setInterval

--- matrices/courses/linkedin-learning/js/node/node-essential-training(2022)/practice/02-globals/timers.js
+++ matrices/courses/linkedin-learning/js/node/node-essential-training(2022)/practice/02-globals/timers.js
@@ -1,7 +1,15 @@
 const waitTime = 3000;
-
 console.log(`setting a ${waitTime / 1000} second delay`);
-
-const timerFinished = () => console.log('done');
-
+const timerFinished = () => {
+    clearInterval(interval);
+    console.log('done');
+}
 setTimeout(timerFinished, waitTime);
+
+const waitInterval = 500;
+let currentTime = 0;
+const incTime = () => {
+    currentTime += waitInterval;
+    console.log(`waiting ${currentTime / 1000} seconds`);
+}
+const interval = setInterval(incTime, waitInterval);
