bb83d6a6e335d7a1bb1d01205d6f79370673118e
Lord Savith <bulletproofchild@gmail.com>
2022-11-11 15:13:16 -0700

    break partials down further and pass along data from res.render()

--- matrices/personal/code/yu-gi-oh-cards/app/routes/indexRouter.js
+++ matrices/personal/code/yu-gi-oh-cards/app/routes/indexRouter.js
@@ -4,7 +4,20 @@ const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res) => {
-    res.render('index', { title: 'Yu-Gi-Oh Cards' });
+    res.render('index', {
+        title: 'Yu-Gi-Oh Cards',
+        cardInfo: {
+            title: 'Castle of Dark Illusions',
+            setCode: 'MRD-073',
+            level: 4,
+            type: 'Flip Effect Monster',
+            race: 'Fiend',
+            attribute: 'Dark',
+            description: 'FLIP: All Zombie - Type monsters gain 200 ATK and DEF. During each of your next 4 Standby Phases, each of those Zombie - Type monsters gains 200 more ATK and DEF. These effects last as long as this card is face - up on the field.',
+            attack: 920,
+            defense: 1930
+        }
+    });
 });
 
 module.exports = router;
