87332059983844a2e91f1228612f437e19d131cc
Lord Savith <bulletproofchild@gmail.com>
2022-11-11 08:36:32 -0700

    ch4 File Management and Streams - writeable file streams

--- matrices/courses/linkedin-learning/js/node/node-essential-training(2022)/practice/04-file-management-streams/ask.js
+++ matrices/courses/linkedin-learning/js/node/node-essential-training(2022)/practice/04-file-management-streams/ask.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+
+let answerStream;
+
 const questions = [
     'What is your name? ',
     'What is your powwer level? ',
@@ -11,17 +15,37 @@ function ask(i) {
     process.stdout.write(`  >  `);
 }
 
-process.stdin.on('data', function (data) {
-    answers.push(data.toString().trim());
-
-    if (answers.length < questions.length) {
-        ask(answers.length);
-    } else {
-        process.exit();
+process.stdin.once('data', (data) => {
+    let name = data.toString().trim();
+    let fileName = `./${name}.md`;
+    if (fs.existsSync(fileName)) {
+        fs.unlinkSync(fileName);
     }
+    answerStream = fs.createWriteStream(fileName);
+    answerStream.write(
+        `Question Answers for ${name}\n========\n`
+    );
+});
+
+process.stdin.on('data', function (data) {
+    let answer = data.toString().trim();
+
+    answerStream.write(
+        `Question: ${questions[answers.length]}\n`
+    );
+
+    answerStream.write(`Answer: ${answer}\n`, function () {
+        if (answers.length < questions.length) {
+            ask(answers.length);
+        } else {
+            process.exit();
+        }
+    });
+    answers.push(answer);
 });
 
 process.on('exit', function () {
+    answerStream.close();
     process.stdout.write('\n\n\n\n  ');
     process.stdout.write(
         `Go ${answers[1]} ${answers[0]} you can finish writing ${answers[2]} later!`
