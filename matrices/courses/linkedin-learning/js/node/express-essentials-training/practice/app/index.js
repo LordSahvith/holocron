9289f1b4e31c2e41c99b0a605b8ad615de6caeb0
Lord Savith <bulletproofchild@gmail.com>
2022-11-11 09:48:14 -0700

    ch2 Express Basics - adding data to server (testing purposes)

--- matrices/courses/linkedin-learning/js/node/express-essentials-training/practice/app/index.js
+++ matrices/courses/linkedin-learning/js/node/express-essentials-training/practice/app/index.js
@@ -1,5 +1,7 @@
 // package.json "type": "module" allows for "import" syntax over "require"
 import express from 'express';
+// assert type needed for experiemental json
+import data from './data/mock.json' assert { type: 'json' };
 
 const app = express();
 const PORT = 3000;
@@ -7,4 +9,5 @@ const PORT = 3000;
 // starts server
 app.listen(PORT, () => {
     console.log(`Express Server running on port:${PORT} `);
+    console.log('data: ', data);
 });
