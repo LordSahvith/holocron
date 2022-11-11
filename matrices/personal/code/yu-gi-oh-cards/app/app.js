1fb5bfe644d4a84579ece7d1b0530f39dc3eb9b3
Lord Savith <bulletproofchild@gmail.com>
2022-11-11 13:50:37 -0700

    get hbs templating setup with partials

--- matrices/personal/code/yu-gi-oh-cards/app/app.js
+++ matrices/personal/code/yu-gi-oh-cards/app/app.js
@@ -1,7 +1,22 @@
 import express from 'express';
 import config from './config';
+import path from 'path';
+import hbs from 'hbs';
+
+// routes
+import indexRouter from './routes/indexRouter';
 
 const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
 
 app.listen(config.port, config.host, () => {
     console.log(`Express listening on port:${config.port}`);
