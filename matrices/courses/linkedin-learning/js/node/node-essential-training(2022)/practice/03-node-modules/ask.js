69e04eeda2240faaa858e0497b7b84894169d8c3
Lord Savith <bulletproofchild@gmail.com>
2022-11-11 08:00:51 -0700

    ch3 Node Modules - using readline

--- matrices/courses/linkedin-learning/js/node/node-essential-training(2022)/practice/03-node-modules/ask.js
+++ matrices/courses/linkedin-learning/js/node/node-essential-training(2022)/practice/03-node-modules/ask.js
@@ -1,10 +1,36 @@
-const readline = require('readline');
-
+const readline = require("readline");
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.question('How do you like Node? ', answer => {
-    console.log(`Your answer: ${answer}`);
+const collectAnswers = (questions, done) => {
+    const answers = [];
+    const [firstQuestion] = questions;
+
+    const questionAnswered = (answer) => {
+        answers.push(answer.trim());
+        if (answers.length < questions.length) {
+            rl.question(
+                questions[answers.length],
+                questionAnswered
+            );
+        } else {
+            return done(answers);
+        }
+    };
+
+    rl.question(firstQuestion, questionAnswered);
+};
+
+const questions = [
+    'What is your name? ',
+    'What is your powwer level? ',
+    'What are you going to do? ',
+];
+
+collectAnswers(questions, (answers) => {
+    console.log("Thank you for your answers!");
+    console.log(answers);
+    process.exit();
 });
