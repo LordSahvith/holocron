6511caf687c9d86c63f8700a69e590c8ae6f9d50
LordSahvith <bulletproofchild@gmail.com>
2023-03-07 06:33:04 -0800

    ch3 objects - references

--- matrices/courses/books/js/js-the-good-parts/ch3-objects.js
+++ matrices/courses/books/js/js-the-good-parts/ch3-objects.js
@@ -183,6 +183,23 @@ console.groupEnd('Update');
  *************/
 console.groupCollapsed('Reference');
 
+const x = stooge;
+x.nickname = 'Curly';
+const nick = stooge.nickname;
+
+// stooge and x refer to the same location in memory so updating one updates both
+console.log(nick); // Curly
+
+const a = {}, b = {}, c = {}; // define 3 different empty objects
+a.test = 'only here'; // define a property
+console.log(a, b, c); // {test: 'only here'} {} {}
+
+const emptyObject = {}; // define empty object
+const ab = emptyObject; // reference empty object
+const bc = emptyObject; // reference empty object
+const cd = emptyObject; // reference empty object
+bc.test = 'only everywhere'; // define a property
+console.log(ab, bc, cd); // {test: 'only everywhere'} {test: 'only everywhere'} {test: 'only everywhere'}
 
 
 console.groupEnd('Reference');
