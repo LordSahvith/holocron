1c0e44f8317a680e6a0132667e7014241d880879
LordSahvith <bulletproofchild@gmail.com>
2023-03-08 10:34:50 -0700

    format

--- matrices/courses/books/js/react/road-to-react/hacker-stories/src/App.jsx
+++ matrices/courses/books/js/react/road-to-react/hacker-stories/src/App.jsx
@@ -3,18 +3,19 @@ import * as React from 'react';
 function App() {
   const welcome = {
     title: 'React',
-    greeting: 'Hey'
+    greeting: 'Hey',
   };
 
   return (
     <div>
-      <h1>{welcome.greeting} {welcome.title}!</h1>
+      <h1>
+        {welcome.greeting} {welcome.title}!
+      </h1>
 
-      <label htmlFor='search'>Search: </label>
-      <input id='search' type='text' />
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
 
 export default App;
-
