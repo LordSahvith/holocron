4de39c7cab41db0ccd2eb2b427035e7609323ac4
LordSahvith <bulletproofchild@gmail.com>
2023-03-08 08:11:55 -0700

    ch1 fundamentals - using JSX

--- matrices/courses/books/js/react/road-to-react/hacker-stories/src/App.jsx
+++ matrices/courses/books/js/react/road-to-react/hacker-stories/src/App.jsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 
 function App() {
-  const title = 'React';
-  
+  const welcome = {
+    title: 'React',
+    greeting: 'Hey'
+  };
+
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>{welcome.greeting} {welcome.title}!</h1>
+
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' />
     </div>
   );
 }
