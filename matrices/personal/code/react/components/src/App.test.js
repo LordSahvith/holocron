8344bcb661ca3e255997efa572aeac828b56fc07
LordSahvith <bulletproofchild@gmail.com>
2023-03-09 15:09:02 -0700

    working jest; set up a few test

--- matrices/personal/code/react/components/src/App.test.js
+++ matrices/personal/code/react/components/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders react modal with open button', () => {
@@ -9,6 +9,14 @@ test('renders react modal with open button', () => {
 
 test('renders and checks for modal', () => {
   render(<App />);
-  const modalMain = document.querySelector('.modal-main')
+  const modalMain = document.querySelector('.modal-main');
   expect(modalMain).toBeInTheDocument();
+});
+
+test('simulates modal button click', () => {
+  render(<App />);
+  const modalButton = document.querySelector('.modal-main button');
+  const modal = document.querySelector('.modal');
+  fireEvent.click(modalButton);
+  expect(modal.classList.contains('display-block'));
 });
