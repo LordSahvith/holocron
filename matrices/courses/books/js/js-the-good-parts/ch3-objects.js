e98c79bfbca802b369a4ba96631b3310dd3a88d4
LordSahvith <bulletproofchild@gmail.com>
2023-05-10 09:59:46 -0600

    ch3 - enumeration

--- matrices/courses/books/js/js-the-good-parts/ch3-objects.js
+++ matrices/courses/books/js/js-the-good-parts/ch3-objects.js
@@ -342,9 +342,9 @@ console.log(another_stooge.profession); // actor
 
 console.groupEnd('Prototype');
 
-/*************
+/**************
  * Reflection *
- *************/
+ **************/
 console.groupCollapsed('Reflection');
 
 // typeof will look at the prototype chain for this property and can produce a value
@@ -363,5 +363,39 @@ console.log(flight.hasOwnProperty('toString')); // false
 console.log(flight.hasOwnProperty('status')); // true
 
 console.groupEnd('Reflection');
+
+/***************
+ * Enumeration *
+ ***************/
+console.groupCollapsed('Enumeration');
+
+// Enumeration: the action of mentioning a number of things one by one.
+console.log('Enumeration: the action of mentioning a number of things one by one.');
+
+// this will loop over all propeties, even those of the prototype, and in no particular order
+// if that's not desired then you should filter out undesired values
+// using hasOwnProperty() method with typeof will help filter out the prototype chain
+let myName;
+for (myName in another_stooge ) {
+  if (typeof another_stooge[myName] !== 'function') {
+    console.log(`${myName}: ${another_stooge[myName]}`);
+  }
+}
+
+console.log('using an array to control order of names:');
+let i;
+let properties = [
+  'first-name',
+  'middle-name',
+  'last-name',
+  'profession'
+];
+// using a 'for' instead of a 'for in' we were able filter out any
+//  values/properties from prototype chain
+for (i = 0; i < properties.length; i++) {
+  console.log(`${properties[i]}: ${another_stooge[properties[i]]}`);
+}
+
+console.groupEnd('Enumeration');
 
 console.groupEnd('Ch3 - Objects');
