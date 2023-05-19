a738634c0c9696cd0fdb71380c1baf64cc20d37c
LordSahvith <bulletproofchild@gmail.com>
2023-05-19 13:36:00 -0600

    ch4 - exceptions

--- matrices/courses/books/js/js-the-good-parts/ch4-functions.js
+++ matrices/courses/books/js/js-the-good-parts/ch4-functions.js
@@ -292,4 +292,32 @@ console.log('test return: ', testReturn3); // { first: 'lord', last: 'savith' }
 
 console.groupEnd('Return');
 
+/**************
+ * Exceptions *
+ **************/
+console.groupCollapsed('Exceptions');
+
+const addException = function (a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw {
+      name: 'TypeError',
+      message: 'add needs numbers'
+    }
+  }
+
+  return a + b;
+};
+
+const try_it = function () {
+  try {
+    addException('seven');
+  } catch (e) {
+    console.log(`${e.name}: ${e.message}`);
+  }
+};
+
+try_it();
+
+console.groupEnd('Exceptions');
+
 console.groupEnd('Ch4 - Functions');
