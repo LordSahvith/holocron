6c07da358775f674f0c602a83a4b067981e0ce55
LordSahvith <bulletproofchild@gmail.com>
2023-05-23 09:05:41 -0600

    ch4 - augmenting types

--- matrices/courses/books/js/js-the-good-parts/ch4-functions.js
+++ matrices/courses/books/js/js-the-good-parts/ch4-functions.js
@@ -320,4 +320,41 @@ try_it();
 
 console.groupEnd('Exceptions');
 
+/********************
+ * Augmenting Types *
+ ********************/
+console.groupCollapsed('Augmenting Types');
+
+Function.prototype.method = function (name, func) {
+  this.prototype[name] = func;
+  return this;
+}
+
+Number.method('integer', function () {
+  return Math[this < 0 ? 'ceil' : 'floor'](this);
+});
+
+console.log((-10 / 3).integer()); // -3
+console.log((10 / -3).integer()); // -3
+console.log((10 / 3).integer()); // 3
+console.log((10).integer()); // 10
+console.log((10 * 4).integer()); // 40
+
+String.method('trim', function () {
+  return this.replace(/^\s+|\s+$/g, '');
+});
+
+console.log(' neat '.trim(), ''); // neat
+
+// conditionally set the method of it's not there
+// this helps prevent cross library discrepancies
+Function.prototype.method2 = function (name, func) {
+  if (!this.prototype[name]) {   
+    this.prototype[name] = func;
+    return this;
+  }
+};
+
+console.groupEnd('Augmenting Types');
+
 console.groupEnd('Ch4 - Functions');
