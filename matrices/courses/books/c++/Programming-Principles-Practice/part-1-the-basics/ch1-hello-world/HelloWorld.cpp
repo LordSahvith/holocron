4612cca9d89e1ee61189c6a8cb5374abe844a149
LordSahvith <bulletproofchild@gmail.com>
2023-05-24 15:57:42 -0600

    ch2 - hello world

--- matrices/courses/books/c++/Programming-Principles-Practice/part-1-the-basics/ch1-hello-world/HelloWorld.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() // C++ programs start by executing the funtion main
-{
-    std::cout << "Hello, World!\n";
-    return 0;
-}
---
+++ matrices/courses/books/c++/Programming-Principles-Practice/part-1-the-basics/ch2-hello-world/HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() // C++ programs start by executing the funtion main
+{
+    cout << "Hello, World!\n";
+    cout << "Hello, Programming!\n";
+    cout << "Here we go!\n";
+    return 0;
+}
