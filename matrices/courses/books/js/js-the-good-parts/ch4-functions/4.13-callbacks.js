6f553fb187cfa34201c55f73447726ce89a65fba
LordSahvith <bulletproofchild@gmail.com>
2023-05-31 09:54:29 -0600

    ch5 - object specifiers

--- matrices/courses/books/js/js-the-good-parts/ch4-functions/4.13-callbacks.js
+++ matrices/courses/books/js/js-the-good-parts/ch4-functions/4.13-callbacks.js
@@ -8,7 +8,7 @@ console.groupCollapsed('Callbacks');
 const apiURL = 'https://pokeapi.co/api/v2/pokemon/charmander';
 const response = fetch(apiURL)
   .then(blob => blob.json())
-  .then(json => console.log('callback (async): ', json)) // {abilities: Array(2), base_experience: 62, forms: Array(1), game_indices: Array(20), height: 6, …}
+  .then(json => console.log('ch4.13 - callback (async): ', json)) // {abilities: Array(2), base_experience: 62, forms: Array(1), game_indices: Array(20), height: 6, …}
   .catch(e => console.log(e.message));
 
 console.log('no callback (sync): ', response); // in frozen state. response pending since it did not wait for server response
---
+++ matrices/courses/books/js/js-the-good-parts/ch5-inheritence/5.0-object-specifiers.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/*******************
+ * Object Specifiers *
+ *******************/
+console.groupCollapsed('Object Specifiers');
+
+const makerNoObjectSpecifier = (name, title, powerLevel) => {
+  console.log('name:', name);
+  console.log('title:', title);
+  console.log('power level:', powerLevel);
+};
+
+const objectNoSpecified = makerNoObjectSpecifier('savith', 'lord', 9000);
+
+
+// if you don't know all the parameters, use an object specifier
+// to extract data
+const makerObjectSpecifier = (obj) => {
+  console.log('name:', obj.name);
+  console.log('title:', obj.title);
+  console.log('power level:', obj.powerLevel);
+  console.log('home world:', obj.homeWorld);
+};
+
+const objectSpecified = makerObjectSpecifier({
+  name: 'savith',
+  title: 'lord', 
+  powerLevel: 9000,
+  homeWorld: 'Dromound Kass'
+});
+
+console.groupEnd('Object Specifiers');
