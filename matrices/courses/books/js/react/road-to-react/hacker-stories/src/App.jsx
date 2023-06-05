08ccaa4ab0ce0e1c2b30a5f4e2265c590c034b58
LordSahvith <bulletproofchild@gmail.com>
2023-06-05 10:55:26 -0600

    ch1.8 - Linting with ESLint

--- matrices/courses/books/js/react/road-to-react/hacker-stories/src/App.jsx
+++ matrices/courses/books/js/react/road-to-react/hacker-stories/src/App.jsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
 
+const title = 'React';
+
 function App() {
-  const welcome = {
-    title: 'React',
-    greeting: 'Hey',
-  };
 
   return (
     <div>
-      <h1>
-        {welcome.greeting} {welcome.title}!
-      </h1>
+      <h1>hello world</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
