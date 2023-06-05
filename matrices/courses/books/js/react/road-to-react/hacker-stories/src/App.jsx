746b2c2e16e6b8dbce7da42e0fc415561b7bc2bf
LordSahvith <bulletproofchild@gmail.com>
2023-06-05 11:30:46 -0600

    ch1.9 - Lists in React

--- matrices/courses/books/js/react/road-to-react/hacker-stories/src/App.jsx
+++ matrices/courses/books/js/react/road-to-react/hacker-stories/src/App.jsx
@@ -1,15 +1,49 @@
 import * as React from 'react';
 
-const title = 'React';
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordal Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 3,
+    objectID: 1,
+  },
+];
 
 function App() {
 
   return (
     <div>
-      <h1>hello world</h1>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr />
+
+      <ul>
+        {list.map(item => {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span> {item.author} </span>
+              <span> {item.num_comments} </span>
+              <span> {item.points} </span>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
