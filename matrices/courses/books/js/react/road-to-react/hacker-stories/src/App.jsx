80318e5f9d0be3da87b144406d2d9ad3640dda1d
LordSahvith <bulletproofchild@gmail.com>
2023-06-05 14:28:22 -0600

    ch1.10 - another react component

--- matrices/courses/books/js/react/road-to-react/hacker-stories/src/App.jsx
+++ matrices/courses/books/js/react/road-to-react/hacker-stories/src/App.jsx
@@ -20,31 +20,44 @@ const list = [
 ];
 
 function App() {
-
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search />
 
       <hr />
 
-      <ul>
-        {list.map(item => {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span> {item.author} </span>
-              <span> {item.num_comments} </span>
-              <span> {item.points} </span>
-            </li>
-          )
-        })}
-      </ul>
+      <List />
     </div>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map(item => {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span> {item.author} </span>
+            <span> {item.num_comments} </span>
+            <span> {item.points} </span>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
 
