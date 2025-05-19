f3199979e8f4c39bbbe071ad2036e1130a640b75
Lord Savith <bulletproofchild@gmail.com>
2025-05-18 21:20:05 -0600

    keep mins under 60

--- matrices/personal/code/Timecode/timer.js
+++ matrices/personal/code/Timecode/timer.js
@@ -6,11 +6,9 @@ let hour = 0;
 let timeInFormat = '';
 let timeOutFormat = '';
 let subTitleFormat = '';
-let finalFormatArray = [
-  ['Timecode In', 'Timecode Out', 'Subtitle']
-];
+const finalFormatArray = [['Timecode In', 'Timecode Out', 'Subtitle']];
 
-for (let x = 0; x < 600; x++) {
+for (let x = 0; x < 601; x++) {
   if (sec === 60) {
     sec = 0;
     min++;
@@ -24,8 +22,19 @@ for (let x = 0; x < 600; x++) {
   let formatedInSec = sec;
   let formatedOutSec = sec + 1;
   let formatedInMin = min;
-  let formatedOutMin = min + 1;
+  let formatedOutMin = min;
   let formatedInHour = hour;
+  let formatedOutHour = hour;
+
+  if (formatedOutSec === 60) {
+    formatedOutSec = 0;
+    formatedOutMin++;
+  }
+
+  if (formatedOutMin === 60) {
+    formatedOutMin = 0;
+    formatedOutHour++;
+  }
 
   if (formatedInSec < 10) {
     formatedInSec = '0' + formatedInSec;
@@ -47,8 +56,12 @@ for (let x = 0; x < 600; x++) {
     formatedInHour = '0' + formatedInHour;
   }
 
-  timeInFormat = formatedInHour + ':' + formatedInMin + ':' + formatedInSec + ':00';
-  timeOutFormat = formatedInHour + ':' + formatedOutMin + ':' + formatedOutSec + ':00';
+  if (formatedOutHour < 10) {
+    formatedOutHour = '0' + formatedOutHour;
+  }
+
+  timeInFormat = formatedInHour + ':' + formatedInMin + ':' + formatedInSec + ':00\t';
+  timeOutFormat = formatedOutHour + ':' + formatedOutMin + ':' + formatedOutSec + ':00\t';
   subTitleFormat =
     hour > 0 ? formatedInHour + ':' + formatedInMin + ':' + formatedInSec : formatedInMin + ':' + formatedInSec;
 
@@ -61,7 +74,7 @@ const tsvData = finalFormatArray.map(row => row.join('\t')).join('\n');
 
 fs.writeFile('timecode.tsv', tsvData, 'utf8', err => {
   if (err) {
-    console.log('Some error');
+    console.log('Some error', err);
   } else {
     console.log('file saved');
   }
