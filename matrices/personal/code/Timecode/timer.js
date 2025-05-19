d8756b292c7296da221891515ac137505ab08244
Lord Savith <bulletproofchild@gmail.com>
2025-05-18 20:20:24 -0600

    DMC Walkthrough assets

---
+++ matrices/personal/code/Timecode/timer.js
@@ -0,0 +1,61 @@
+let fs = require('fs');
+
+let sec = 0;
+let min = 0;
+let hour = 0;
+let timeInFormat = '';
+let timeOutFormat = '';
+let subTitleFormat = '';
+let finalFormatArray = [['Timecode In', 'Timecode Out', 'Subtitle']];
+
+for (let x = 0; x < 11; x++) {
+  if (sec === 60) {
+    sec = 0;
+    min++;
+  }
+
+  if (min === 60) {
+    min = 0;
+    hour++;
+  }
+
+  let formatedInSec = sec;
+  let formatedOutSec = sec + 1;
+  let formatedInMin = min;
+  let formatedInHour = hour;
+
+  if (formatedInSec < 10) {
+    formatedInSec = '0' + formatedInSec;
+  }
+
+  if (formatedOutSec < 10) {
+    formatedOutSec = '0' + formatedOutSec;
+  }
+
+  if (formatedInMin < 10) {
+    formatedInMin = '0' + formatedInMin;
+  }
+
+  if (formatedInHour < 10) {
+    formatedInHour = '0' + formatedInHour;
+  }
+
+  timeInFormat = formatedInHour + ':' + formatedInMin + ':' + formatedInSec + ':00\u0009';
+  timeOutFormat = formatedInHour + ':' + formatedInMin + ':' + formatedOutSec + ':00\u0009';
+  subTitleFormat =
+    hour > 0 ? formatedInHour + ':' + formatedInMin + ':' + formatedInSec : formatedInMin + ':' + formatedInSec;
+
+  finalFormatArray.push([timeInFormat + timeOutFormat + subTitleFormat]);
+
+  sec++;
+}
+
+const tsvData = finalFormatArray.map(row => row.join('\t')).join('\n');
+
+fs.writeFile('timecode.tsv', tsvData, 'utf8', err => {
+  if (err) {
+    console.log('Some error');
+  } else {
+    console.log('file saved');
+  }
+});
